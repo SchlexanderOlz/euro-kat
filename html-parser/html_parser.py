0d54c8fef59a05524e4aedd6edd54697bd47ff3a
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-06-06 21:36:25 +0200

    added extra depth for linked files (concept)

--- html-parser/html_parser.py
+++ html-parser/html_parser.py
@@ -1,38 +1,57 @@
 from bs4 import BeautifulSoup
 import os
 
-class HTMLContentExtractor:
+class InformationExtractor:
     @staticmethod
     def replace_nbsp_elements(html):
         soup = BeautifulSoup(html, 'html.parser')
+        
+        # This get's all elements in the html which are not a non-breaking-space (&nbsp)
+        # They then get replaced by ??? just for simnplified output
         for element in soup.find_all(text=lambda t: t == '\xa0'):
             element.replace_with('???')
         return str(soup)
 
+
+    # TODO Rewrite cleanup part so always the last serial-element is referenced (currently printing "Not implemented!")
     @staticmethod
-    def get_html_content(path: str, html_struct: str, clazz: str = None) -> list[str, list[str]]:
+    def get_html_content(path: str, html_struct: str, clazz: str = None) -> list[str, list[str, bool]]:
+        html: str = ""
         with open(path) as file:
             html = file.read()
 
-            replaced_html = HTMLContentExtractor.replace_nbsp_elements(html)
-            soup = BeautifulSoup(replaced_html, 'html.parser')
-            td_elements = soup.find_all(html_struct, class_=clazz)
-            content = []
-            for td in td_elements:
-                text = td.get_text(strip=True, separator=' ')
-                link = td.find('a')
-                content.append(text)  # Content is appended to position 0
+        replaced_html = InformationExtractor.replace_nbsp_elements(html)
+        soup = BeautifulSoup(replaced_html, 'html.parser')
+        td_elements = soup.find_all(html_struct, class_=clazz)
+        content = []
+        for td in td_elements:
+            text = td.get_text(strip=True, separator=' ')
+            content.append(text)
+            
+            link = td.find('a')
+            if link:
+                href = link.get('href')
+                joined_href = InformationExtractor.join_paths(href, os.path.dirname(path))
+                content.append(InformationExtractor.get_figure_content(joined_href))  # Append link content instead of link directly
 
-                if link:
-                    href = link.get('href')
-                    joined_href = HTMLContentExtractor.join_paths(href, os.path.dirname(path))
-                    content.append(HTMLContentExtractor.get_figure_content(joined_href))  # Append link content instead of link directly
+        formatted = []
+        last_serial: str = "Not implemented!"
+        for value in content:
+            if not isinstance(value, str):
+                formatted.append(value)
+                continue
+            
+            value = InformationExtractor.cleanup(value)
+            if value == '"':
+                value = last_serial
+            
+            formatted.append(value)
 
-        return [value.replace('\n', '').replace('\t', '').replace('"', '???') if isinstance(value, str) else value for value in content]
+        return formatted
 
 
     @staticmethod
-    def structure_content(content: list[str, list[str]], break_at: str, group_range: int = 1, ignore: set[str] = None, link_pos: int = 1) -> list[list[str, list[str]]]:
+    def structure_content(content: list[str, list[str]], break_at: str, group_range: int = 1, ignore: set[str] = None, link_pos: int = 1) -> list[list[str, list[str, bool]]]:
         
         group_range += 1
 
@@ -63,6 +82,26 @@ class HTMLContentExtractor:
 
 
     @staticmethod
-    def get_figure_content(href: str) -> list[str]:
-        print(href)
-        return ["Figure Content"]
+    def get_figure_content(href: str) -> list[str, bool]:
+        html: str = ""
+        with open(href) as file:
+            html = file.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        
+        # TODO Make function instead which checks if the value is valid
+        kennung_structs = soup.find_all('td', string=lambda text: text and text.strip() == 'Kennung')
+        aufkleber_structs = soup.find_all('td', string=lambda text: text and text.strip() == 'Aufkleber')
+        
+        values = []
+        for i in range(len(kennung_structs)):
+            kennung = InformationExtractor.cleanup(kennung_structs[i].find_next('td').get_text(strip=True))
+            aufkleber = InformationExtractor.cleanup(aufkleber_structs[i].find_next('td').get_text(strip=True)) != 'keine Aufkleber'
+            
+            values.append([kennung, aufkleber])
+                    
+        return values
+    
+    
+    @staticmethod
+    def cleanup(val: str):
+        return val.replace('\n', '').replace('\t', '')
--- html-parser/scrape_content.py
+++ html-parser/scrape_content.py
@@ -1,4 +1,4 @@
-from html_parser import HTMLContentExtractor
+from html_parser import InformationExtractor
 
 SRCFILE: str = "I:/Code/(Deprecated)EuroKatFiles/JGListen/WSE.htm"
 
@@ -8,9 +8,10 @@ class Scraper:
         content: str
         with open(SRCFILE) as file:
             html = file.read()
-            content = HTMLContentExtractor.get_html_content(SRCFILE, 'td', 'a')
-        return HTMLContentExtractor.structure_content(content, "Sonstiges aus der Saison 2022-2023 Deutschland", 4)
+            content = InformationExtractor.get_html_content(SRCFILE, 'td', 'a')
+        return InformationExtractor.structure_content(content, "Sonstiges aus der Saison 2022-2023 Deutschland", 4)
 
 
 if __name__ == "__main__":
     print(Scraper.get_html_content())
+    #Scraper.get_html_content()
