84847d8ef0ac3cb32cfee57f8d7e0500f775ccd9
LiamKrenn <k.liam1224@gmail.com>
2023-06-26 12:22:00 +0200

    Landing page

--- svelte-kat/src/routes/+page.svelte
+++ svelte-kat/src/routes/+page.svelte
@@ -1,13 +1,73 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+</script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-		</ul>
-	</div>
+<div class="p-3">
+	<h1 class="h1">Lorem Ipsum</h1>
+
+	<p class="my-5">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt, augue nec pharetra
+		tincidunt, augue erat posuere purus, eget accumsan metus purus ut velit. Mauris vitae justo nec
+		libero consectetur vehicula quis in sapien. Nullam eu faucibus massa. Nulla luctus purus euismod
+		sem facilisis blandit. Aliquam lobortis consectetur sodales. Sed pretium laoreet eleifend.
+		Quisque facilisis accumsan justo, vel porttitor odio facilisis dignissim. Maecenas accumsan nunc
+		id sem maximus, sed interdum mi elementum. Donec sagittis diam eget purus placerat, vel
+		malesuada magna dapibus. Suspendisse eget diam vel mi viverra tempus a vitae neque. Nunc
+		pharetra mi nec nunc sodales, nec scelerisque lorem efficitur.
+	</p>
+	<p class="my-5">
+		Maecenas velit eros, lobortis ut rhoncus nec, volutpat egestas ligula. Praesent id erat eget
+		arcu dignissim placerat et eu elit. Morbi nec dapibus odio, nec hendrerit nunc. Etiam feugiat,
+		nisi a ultricies commodo, velit augue ultrices dolor, ac imperdiet leo odio a arcu. Fusce
+		ultrices tempor sem, vitae suscipit lorem vestibulum non. Ut euismod augue leo, eget maximus
+		metus gravida ac. Morbi iaculis, neque id suscipit mollis, justo eros faucibus ipsum, vel
+		pellentesque enim ex quis felis.
+	</p>
+	<p class="my-5">
+		Donec consectetur facilisis rutrum. Duis at mattis tortor. Etiam nec lectus finibus, feugiat
+		tellus id, laoreet odio. Sed laoreet mauris lorem, vel fringilla sapien pharetra a. Quisque eget
+		nibh tincidunt, aliquam eros quis, tristique lorem. Fusce semper nulla eget nulla rutrum
+		venenatis. Quisque facilisis lacus sit amet fermentum tincidunt. Nulla varius metus ante, eget
+		molestie felis imperdiet non. Suspendisse potenti. Donec odio risus, tristique non diam sed,
+		congue molestie elit. Cras tincidunt sollicitudin ullamcorper. Vestibulum nec convallis leo.
+		Etiam maximus lorem sit amet tincidunt blandit. Aliquam erat volutpat. Duis tristique odio ut
+		vehicula efficitur.
+	</p>
+	<p class="my-5">
+		Vivamus ligula lectus, tempor a nisl a, condimentum elementum erat. Mauris quis iaculis orci.
+		Etiam erat enim, vehicula ac malesuada ut, egestas ut justo. Donec non dictum justo, ut viverra
+		quam. In sodales hendrerit semper. Pellentesque vitae convallis est. Suspendisse at fermentum
+		lorem, a ultricies mauris. Mauris laoreet felis commodo nunc luctus malesuada. Aenean vehicula
+		nisi non tellus vehicula, at mollis purus bibendum. Nullam nulla turpis, vulputate eu mollis et,
+		tincidunt vitae odio. Donec placerat neque sed justo elementum gravida. Vestibulum venenatis
+		augue nec ligula vulputate accumsan. Curabitur viverra, felis congue aliquet placerat, neque
+		purus dapibus purus, quis mattis lacus nisi dignissim ipsum. Vivamus aliquet felis quis feugiat
+		convallis. Nunc tincidunt commodo libero, sit amet fringilla ex accumsan ac.
+	</p>
+	<p class="my-5">
+		Nulla vulputate elit a sodales dignissim. Vivamus aliquet euismod dapibus. Etiam tincidunt
+		tellus ut nulla ullamcorper, eu consectetur libero lacinia. Sed nisi lorem, sollicitudin sit
+		amet odio eu, vehicula sodales sem. Curabitur aliquet non orci vel blandit. Nulla commodo erat
+		velit, sed sollicitudin lacus lobortis eget. Etiam non sapien massa.
+	</p>
+	<p class="my-5">
+		Duis at malesuada erat. Sed ultrices tellus vel odio ultricies vestibulum. Nullam faucibus velit
+		orci, ut pellentesque sem feugiat ut. Duis viverra mauris cursus enim fringilla, vitae ultricies
+		tellus consectetur. Phasellus ultrices eros libero, eget faucibus nulla venenatis nec. Maecenas
+		ipsum nulla, mollis in feugiat condimentum, suscipit at ligula. Curabitur commodo efficitur dui
+		in faucibus. Ut lacinia mi arcu, at varius ligula eleifend vitae. Integer faucibus molestie
+		metus ac rutrum. Sed egestas bibendum metus ut convallis. Vivamus lobortis faucibus dui. Aliquam
+		erat volutpat.
+	</p>
+	<p class="my-5">
+		Suspendisse malesuada ante non neque rhoncus porttitor. Aliquam sed volutpat elit. Maecenas
+		tempus luctus ex, eget facilisis neque rutrum in. Integer porta sed eros vel sodales. Etiam non
+		pulvinar nibh. Sed vestibulum in odio id scelerisque. Pellentesque sodales at dolor maximus
+		scelerisque. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+	</p>
+	<p class="my-5">
+		Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer et accumsan ante.
+		Curabitur non ultricies nulla. Maecenas eget nulla at tellus faucibus volutpat. Sed interdum
+		iaculis viverra. Nunc molestie neque et dignissim luctus. Proin vitae massa eget dolor congue
+		lacinia et id nunc.
+	</p>
 </div>
---
+++ svelte-kat/src/routes/Nav.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { ListBox, ListBoxItem, popup } from '@skeletonlabs/skeleton';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
+
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import DownIcon from './DownIcon.svelte';
+	
+	export let title: string;
+	export let options: string[];
+	let testvalue = '';
+
+	const popupCombobox: PopupSettings = {
+		event: 'click',
+		target: title + 'navbar',
+		closeQuery: '.listbox-item',
+		placement: 'bottom'
+	};
+</script>
+
+<button class="btn variant-ringed select-none" use:popup={popupCombobox}>
+	{title}
+	<DownIcon/>
+</button>
+<div class="card shadow-xl py-1 select-none" data-popup="{title}navbar">
+	<ListBox rounded="rounded-none">
+		{#each options as option}
+			<ListBoxItem
+        class="rounded-lg text-sm "
+        active="hover:variant-soft"
+				bind:group={testvalue}
+				name="medium"
+				value={option}
+				on:click={() => {}}>{option}</ListBoxItem
+			>
+		{/each}
+	</ListBox>
+</div>
