3c02ab071ee27d122a31db633c6a1b7238257b4f
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-08-05 20:32:58 +0200

    Added db-accesss mockup in lib folder

--- sveltekat/src/lib/PocketBase.ts
+++ sveltekat/src/lib/PocketBase.ts
@@ -1,12 +1,90 @@
 // https://pocketbase.io/docs/api-records/ <- docs
 
 import PocketBase from 'pocketbase';
-import type { Partner } from './Types';
+import type {
+	Series,
+	SubSeries,
+	Country,
+	Packaging,
+	Figure,
+	Variation,
+	CountryVariation,
+	PackageInsert
+} from './Types';
+
+const country: Country = {
+	id: 5,
+	countryName: 'Austria'
+};
+
+const packaging: Packaging = {
+	description: 'This is a package',
+	pictures: ['binary code', 'other binary code']
+};
+
+const variation: Variation = {
+	variationId: 10,
+	variation: 'Has a cock',
+	picture: 'a picture of variation'
+};
+
+const countryVariation: CountryVariation = {
+	countryVariationId: 10,
+	country: country,
+	year: 1999,
+	note: 'This is a variaiton'
+};
+
+const packageInserts: PackageInsert = {
+	description: 'This ís the basic package_insert',
+	picture: ['This is a picture']
+};
+
+const figures: Figure[] = [
+	{
+		figureId: 10,
+		mpgNr: 2349,
+		figureName: 'Deine mum',
+		fake: true,
+		sticker: false,
+		identifier: true,
+		note: 'Nice cook',
+		variation: variation,
+		pictures: ['a picture 1', 'a picture 2'],
+		countryVariations: [countryVariation],
+		packageInserts: [packageInserts, packageInserts]
+	}
+];
+
+const subSeries: SubSeries = {
+	subSeriesId: 420,
+	serialName: 'Deine mum',
+	country: country,
+	thanks: 'Thank you!!!!',
+	releaseYear: 420,
+	packaging: packaging,
+	figures: figures
+};
+const series: Series = {
+	seriesId: 69,
+	idLetters: 'mpkg',
+	year: 1420,
+	subSeries: [subSeries, subSeries]
+};
 
 const pb: PocketBase = new PocketBase('http://127.0.0.1:8090');
 
-export async function getPartners(): Promise<Partner[]> {
-  const result: Partner[] = await pb.collection('partner').getFullList<Partner>();
-  const json: Partner[] = structuredClone(result);
-  return json
+export async function getSubSeriesByLetter(seriesLetter: string): Promise<SubSeries> {
+	if (seriesLetter === 'soos') {
+		throw TypeError('Damn mann you litttle suusy bucka');
+	}
+	return subSeries;
+}
+
+export async function getSeries(): Promise<Series> {
+	return series;
+}
+
+export async function getFigureOfSeries(series: string) {
+	return figures;
 }
--- sveltekat/src/lib/Types.ts
+++ sveltekat/src/lib/Types.ts
@@ -1,23 +1,64 @@
-export interface Partner {
-	collectionId: string;
-	collectionName: string;
-	created: string;
-	description: string;
-	id: string;
-	link: string;
-	logo: string;
-	logourl: string;
-	name: string;
-	updated: string;
+export interface Figure {
+	figureId: Number;
+	mpgNr: Number;
+	figureName: string;
+	fake: boolean;
+	sticker: boolean;
+	identifier: boolean;
+	note: string;
+	variation: Variation;
+	pictures: string[]; // Pictues are Base64 encoded
+	countryVariations: CountryVariation[];
+	packageInserts: PackageInsert[];
 }
 
-export interface Times {
-	collectionId: string;
-	collectionName: string;
-	id: string;
-	created: string;
-	updated: string;
-	from: string;
-	to: string;
-	event: string;
+export interface CountryVariation {
+	countryVariationId: Number;
+	country: Country;
+	year: Number;
+	note: string;
+}
+
+export interface PackageInsert {
+	description: string;
+	picture: string[]; // Pictures are Base64 encoded
+}
+
+export interface SubSeries {
+	subSeriesId: Number;
+	serialName: string;
+	country: Country;
+	thanks: string;
+	releaseYear: Number;
+	packaging: Packaging;
+	figures: Figure[];
+}
+
+export interface Packaging {
+	description: string;
+	pictures: string[]; // Pictures are Base64 encoded
+}
+
+export interface Series {
+	seriesId: Number;
+	idLetters: string;
+	year: Number;
+	subSeries: SubSeries[];
+}
+
+export interface Variation {
+	variationId: Number;
+	variation: string;
+	picture: string; // Base64 string
+}
+
+export interface Series {
+	seriesId: Number;
+	idLetters: string;
+	year: Number;
+}
+
+export interface Country {
+	id: Number;
+	countryName: string;
 }
