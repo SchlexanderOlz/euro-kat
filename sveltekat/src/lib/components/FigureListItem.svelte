bd94cedf5625c8f3ed7c437d01afe3b685ce881a
LiamKrenn <k.liam1224@gmail.com>
2023-08-15 00:13:59 +0200

    made prototype a bit fancier

--- sveltekat/src/lib/components/FigureListItem.svelte
+++ sveltekat/src/lib/components/FigureListItem.svelte
@@ -4,4 +4,10 @@
   export let figure: Figure;
 </script>
 
-<p>{figure.figureName}</p>
+<div class="card py-1 px-2 my-1.5 flex justify-between">
+  <div class="flex">
+    <p class="w-32">{figure.figureName}</p>
+    <p class="opacity-50">Note: {figure.note}</p>
+  </div>
+  <p class="w-32 text-right"><span class="opacity-50">Nr: </span>#{figure.figureId}</p>
+</div>
