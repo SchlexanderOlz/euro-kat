2a597d4704fe73ab570e0a81eee466859cf4120c
LiamKrenn <k.liam1224@gmail.com>
2023-08-15 00:06:00 +0200

    Figure Page

---
+++ sveltekat/src/routes/figures/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { figures } from '$lib/PocketBase';
+	import FigureList from '$lib/components/FigureList.svelte';
+	import FigureListItem from '$lib/components/FigureListItem.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="h-full w-full px-16 pt-12">
+	<FigureList>
+		{#each figures as figure (figure.figureId)}
+			<FigureListItem {figure} />
+		{/each}
+	</FigureList>
+</div>
