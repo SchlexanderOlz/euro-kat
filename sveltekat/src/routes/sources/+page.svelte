d83e1e74ffa9567e701291ce3b8688dbd3984552
LiamKrenn <k.liam1224@gmail.com>
2023-09-07 03:38:15 +0200

    sources page

---
+++ sveltekat/src/routes/sources/+page.svelte
@@ -0,0 +1,35 @@
+<div class="w-full flex flex-col items-center mt-6 px-[10%] cursor-default">
+	<h1 class="h1 mb-5 select-none">Quellen</h1>
+	<p class="text-xl mt-2">Kataloge und Zeitschriften:</p>
+	<p class="mt-2">
+		Neben dem jeweils aktuellem "<span class="font-bold">O-Ei-A</span>" -
+		<a href="http://www.feiler-verlag.de/">Feiler-Verlag</a>
+	</p>
+	<p class="mt-2">
+		verwend ich hauptsächlich die Jahrgangskataloge von <span class="font-bold"
+			>Meinhard Foethke</span
+		>,
+	</p>
+	<a href="http://komax38.de/index.html"
+		><p class="underline mt-2">
+			<span class="font-bold">Max Köppl's</span> Jahrgangsübersichten
+		</p></a
+	>
+	<p>und</p>
+	<a href="https://www.yumpu.com/de/ferrero"
+		><p class="underline"><span class="font-bold">Achim Grundgeiger's</span> Online Kataloge</p></a
+	>
+	<p class="mt-4 text-center">
+		Etwas veraltet aber immer noch gut :<br />
+		SU Katalog - Spielzeug aus dem Ei - Fantasia Verlag<br />
+		1. Deutsche Ü-Ei Katalog (Krägermann) 2001<br />
+		Katalog von Schrage 1999/2000<br />
+		Welz Ü-Ei Figurenkatalog 2000<br />
+		Guida Catalog per il Collezionta 2000<br />
+		Kataloge Hansen und Schade<br />
+	</p>
+	<p class="mt-4">
+		Bei den Zeitschriften greife ich auf die Ausgaben "Das Ei", "Sammler-Hit" und "Sammlercosmos".
+		zurück.
+	</p>
+</div>
