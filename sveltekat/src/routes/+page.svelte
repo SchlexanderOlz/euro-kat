366a1fe3550fc76e74a67a27db4f92e7250471bc
LiamKrenn <k.liam1224@gmail.com>
2023-09-07 02:19:32 +0200

    homepage

--- sveltekat/src/routes/+page.svelte
+++ sveltekat/src/routes/+page.svelte
@@ -3,14 +3,34 @@
 
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-		</ul>
+<h1 class="h1 text-8xl w-full justify-center flex mt-4 mb-8">Euro-Katalog</h1>
+
+<div class="w-full flex items-center flex-col">
+	<div class="flex card p-2 mx-4 mt-4 w-[60%]">
+		<img class="h-36" alt="helmut" src="images/helmut.jpg" />
+		<div class="ml-4 text-xl">
+			<p class="underline">Herzlich Willkommen auf diesen Sammlerseiten!</p>
+			<p>welcome to these collector's sides!</p>
+			<p>Cordialement bienvenue sur ces côtés de collectionneur</p>
+			<p>Una cordial bienvenida a todos los coleccionistas de Kinder a estas páginas!</p>
+			<p>Добро пожаловать на сайт коллекционеров!</p>
+		</div>
 	</div>
+	<p class="mt-8 text-2xl">
+		Der <span class="font-bold">Euro-Katalog</span> als die umfangreichste Informationssammlung zu den
+		Ü-Ei-Inhalten der vergangenen Jahre.
+	</p>
+	<p class="mt-8 w-[60%]">
+		Die Sammlerseiten leben von der Unterstützung durch Sammlerinnen und Sammler aus der ganzen
+		Welt. Es ist mir wichtig, mich für jede zugesendete Information, jedes zugesendete Bild zu
+		bedanken indem ich Name und Vorname der Zusenderin/des Zusenders auf der jeweiligen Seite
+		angebe. Dieses wende ich jetzt über 10 Jahre an und ist für alle Besucherinnen und Besucher
+		meiner Seiten sichtbar.
+		<span class="opacity-40">
+			ACHTUNG! Mit der Zusendung einer Information, eines Bildes erklären Sie sich mit der
+			Veröffentlich Ihres Namen und Ihres Vornamens einverstanden! Sollten Sie dieses nicht wünschen,
+			so teilen Sie mir dieses in der Mail mit.
+		</span>
+	</p>
+	<p class="absolute bottom-32">Weitere Informationen und die DSGVO konforme Datenschutzerklärung finden Sie unter <a class="underline" href="/impressum">Impressum/Datenschutz.</a></p>
 </div>
