7a0170fb6e21db91482aa481326e5f97f4c5dd5d
LiamKrenn <k.liam1224@gmail.com>
2023-09-07 17:48:45 +0200

    responsiveness

--- sveltekat/src/routes/+page.svelte
+++ sveltekat/src/routes/+page.svelte
@@ -3,34 +3,43 @@
 
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<h1 class="h1 text-8xl w-full justify-center flex mt-6 mb-8 cursor-default">Euro-Katalog</h1>
 
-<div class="w-full flex items-center flex-col cursor-default">
-	<div class="flex card p-2 mx-4 mt-4 w-[60%]">
-		<img class="h-36" alt="helmut" src="images/helmut.jpg" />
-		<div class="ml-4 text-xl">
-			<p class="underline">Herzlich Willkommen auf diesen Sammlerseiten!</p>
-			<p>welcome to these collector's sides!</p>
-			<p>Cordialement bienvenue sur ces côtés de collectionneur</p>
-			<p>Una cordial bienvenida a todos los coleccionistas de Kinder a estas páginas!</p>
-			<p>Добро пожаловать на сайт коллекционеров!</p>
-		</div>
+
+<div class="w-full flex flex-col items-center mb-8">
+	<div class="text-xl mt-3 opacity-60 text-center">
+		<p>Willkommen! Welcome! Accueillir! Bienvenido! приветствие!</p>
 	</div>
-	<p class="mt-8 text-2xl">
-		Der <span class="font-bold">Euro-Katalog</span> als die umfangreichste Informationssammlung zu den
-		Ü-Ei-Inhalten der vergangenen Jahre.
-	</p>
-	<p class="mt-8 w-[60%]">
-		Die Sammlerseiten leben von der Unterstützung durch Sammlerinnen und Sammler aus der ganzen
-		Welt. Es ist mir wichtig, mich für jede zugesendete Information, jedes zugesendete Bild zu
-		bedanken indem ich Name und Vorname der Zusenderin/des Zusenders auf der jeweiligen Seite
-		angebe. Dieses wende ich jetzt über 10 Jahre an und ist für alle Besucherinnen und Besucher
-		meiner Seiten sichtbar.
-		<span class="opacity-40">
-			ACHTUNG! Mit der Zusendung einer Information, eines Bildes erklären Sie sich mit der
-			Veröffentlich Ihres Namen und Ihres Vornamens einverstanden! Sollten Sie dieses nicht wünschen,
-			so teilen Sie mir dieses in der Mail mit.
-		</span>
-	</p>
-	<p class="mt-8">Weitere Informationen und die DSGVO konforme Datenschutzerklärung finden Sie unter <a class="underline" href="/data-protection">Datenschutz.</a></p>
+	<div class="flex items-center flex-col cursor-default lg:w-[75%] w-[90%]">
+		<h1 class="h1 md:text-8xl text-5xl h-full flex mt-5  cursor-default text-center">Euro-Katalog</h1>
+		<p class="mt-8 text-2xl text-center">
+			Der <span class="font-bold">Euro-Katalog</span> als die umfangreichste Informationssammlung zu
+			den Ü-Ei-Inhalten der vergangenen Jahre.
+			
+		</p>
+
+		<div class="flex sm:flex-row flex-col card p-2 mx-4 mt-8 max-w-[1166px]">
+			<img
+				height="116px"
+				width="101px"
+				class="sm:h-36 sm:w-auto w-40 inline"
+				alt="helmut"
+				src="images/helmut.jpg"
+			/>
+
+			<p class="sm:ml-4 sm:mt-0 mt-1">
+				Die Sammlerseiten leben von der Unterstützung durch Sammlerinnen und Sammler aus der ganzen
+				Welt. Es ist mir wichtig, mich für jede zugesendete Information, jedes zugesendete Bild zu
+				bedanken indem ich Name und Vorname der Zusenderin/des Zusenders auf der jeweiligen Seite
+				angebe. Dieses wende ich jetzt über 10 Jahre an und ist für alle Besucherinnen und Besucher
+				meiner Seiten sichtbar.
+			</p>
+		</div>
+
+		<p class="mt-8 text-center">
+			Weitere Informationen und die DSGVO konforme Datenschutzerklärung finden Sie unter <a
+				class="underline"
+				href="/data-protection">Datenschutz.</a
+			>
+		</p>
+	</div>
 </div>
