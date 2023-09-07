83822bf6d5cb346fa8a78cdfe169debd3c074f34
LiamKrenn <k.liam1224@gmail.com>
2023-09-07 13:36:33 +0200

    impressum

---
+++ sveltekat/src/routes/imprint/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+</script>
+
+
+<div class="w-full flex flex-col items-center text-left mt-6 px-[10%] cursor-default">
+	<h1 class="h1 mb-5 select-none">Impressum</h1>
+    <div class="w-max text-left">
+        <p>Betreiber und verantwortlicher im datenschutzrechtlichen Sinne:<br/>
+            Helmut J.F.Henze<br/>
+            Lüder-Bömermann-Str. 30<br/>
+            D-28777 Bremen<br/>
+            E-Mail: Sammler@HJFHenze.de<br/>
+            <br/>
+            <br/>
+            Verantwortlich für redaktionelle Beiträge und Datenschutz:<br/>
+            Helmut J.F.Henze<br/>
+            <br/>
+            zugehörige Domain:	 <br/>
+            <div class="flex">
+                <p class="mr-3">
+                    www.HJFHenze.de	           <br/>
+                    www.Euro-Kat.de	           <br/>
+                    www.FF-Kat.de	           <br/>
+                    www.Sammelbilder-Katalog.de<br/>
+                </p>
+                <p>
+                     Helmut's Sammlerseiten<br/>
+                     Der Euro-Katalog<br/>
+                     Der Fremdfiguren-Katalog<br/>
+                     Der Sammel-/Kaufmannsbilder-Katalog<br/></p>
+            </div>
+    </div>
+    
+</div>
+
+
