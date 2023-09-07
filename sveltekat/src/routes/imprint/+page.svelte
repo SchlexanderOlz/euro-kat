399362a2258c02ccf8474971b3ff1404606416e2
LiamKrenn <k.liam1224@gmail.com>
2023-09-07 19:10:50 +0200

    imprint site responsiveness

--- sveltekat/src/routes/imprint/+page.svelte
+++ sveltekat/src/routes/imprint/+page.svelte
@@ -1,37 +1,34 @@
 <script lang="ts">
 </script>
 
-
-<div class="w-full flex flex-col items-center text-left mt-6 px-[10%] cursor-default text-surface-900-50-token">
-	<h1 class="h1 mb-5 select-none">Impressum</h1>
-    <p class="text-red-600 text-3xl w-full flex justify-center font-bold">Bitte drüber lesen</p>
-    <div class="w-max text-left">
-        <p>Betreiber und verantwortlicher im datenschutzrechtlichen Sinne:<br/>
-            Helmut J.F.Henze<br/>
-            Lüder-Bömermann-Str. 30<br/>
-            D-28777 Bremen<br/>
-            E-Mail: Sammler@HJFHenze.de<br/>
-            <br/>
-            <br/>
-            Verantwortlich für redaktionelle Beiträge und Datenschutz:<br/>
-            Helmut J.F.Henze<br/>
-            <br/>
-            zugehörige Domain:	 <br/>
-            <div class="flex">
-                <p class="mr-3">
-                    www.HJFHenze.de	           <br/>
-                    www.Euro-Kat.de	           <br/>
-                    www.FF-Kat.de	           <br/>
-                    www.Sammelbilder-Katalog.de<br/>
-                </p>
-                <p>
-                     Helmut's Sammlerseiten<br/>
-                     Der Euro-Katalog<br/>
-                     Der Fremdfiguren-Katalog<br/>
-                     Der Sammel-/Kaufmannsbilder-Katalog<br/></p>
-            </div>
-    </div>
-    
+<div
+	class="w-full mt-6 px-[10%] cursor-default text-surface-900-50-token"
+>
+	<h1 class="h1 mb-5 select-none w-full flex justify-center">Impressum</h1>
+	<p class="text-red-600 text-3xl w-full flex justify-center font-bold">Bitte drüber lesen</p>
+	<div class="text-left">
+		<p>
+			Betreiber und verantwortlicher im datenschutzrechtlichen Sinne:<br />
+			Helmut J.F.Henze<br />
+			Lüder-Bömermann-Str. 30<br />
+			D-28777 Bremen<br />
+			E-Mail: Sammler@HJFHenze.de<br />
+			<br />
+			<br />
+			Verantwortlich für redaktionelle Beiträge und Datenschutz:<br />
+			Helmut J.F.Henze<br />
+			<br />
+			zugehörige Domain: <br />
+		</p>
+		<p class="mr-3">
+			Helmut's Sammlerseiten:<br />
+			<span class="font-semibold">www.HJFHenze.de</span><br />
+			Der Euro-Katalog:<br />
+			<span class="font-semibold">www.Euro-Kat.de</span><br />
+			Der Fremdfiguren-Katalog:<br />
+			<span class="font-semibold">www.FF-Kat.de</span><br />
+			Der Sammel-/Kaufmannsbilder-Katalog:<br />
+			<span class="font-semibold">www.Sammelbilder-Katalog.de</span>
+		</p>
+	</div>
 </div>
-
-
