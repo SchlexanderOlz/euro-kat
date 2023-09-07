30cd6b739fff440385bddf6af730cc854b4ed913
LiamKrenn <k.liam1224@gmail.com>
2023-09-07 02:19:39 +0200

    fixed visual bug

--- sveltekat/src/lib/components/FigureListItem.svelte
+++ sveltekat/src/lib/components/FigureListItem.svelte
@@ -7,7 +7,7 @@
 <div class="card py-1 px-2 my-1.5 flex justify-between">
   <div class="flex">
     <p class="w-32">{figure.figureName}</p>
-    <p class="opacity-50">Note: {figure.note}</p>
+    <p class="text-surface-400">Note: {figure.note}</p>
   </div>
-  <p class="w-32 text-right"><span class="opacity-50">Nr: </span>#{figure.mpgNr}</p>
+  <p class="w-32 text-right"><span class="">Nr: </span>#{figure.mpgNr}</p>
 </div>
