ee1968293948d0f047fc06190fb3a118d961e350
LiamKrenn <k.liam1224@gmail.com>
2023-09-07 02:39:53 +0200

    info page

---
+++ sveltekat/src/routes/info/+page.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+</script>
+
+<div class="w-full flex flex-col items-center mt-6 px-[10%]">
+	<h1 class="h1">Etwas über diese Seite</h1>
+	<p class="mt-8">
+		Seit über 10 Jahren gibt es "<span class="font-bold">Helmut's Sammlerseiten</span>" als
+		Katalogseiten für Jahrgangssammler D und EU. Angefangen mit reinen Excel-Jahrgangslisten hat
+		sich in relativ kurzer Zeit, auch dank kräftiger Unterstützung durch viele Sammler aus dem In-
+		und Ausland, einer der größten bebilderten Kataloge der Inhalte aus dem Überraschungsei
+		entwickelt. Ende 2007 hat ich den "<span class="font-bold">Euro-Kat</span>", an dem ich bereits
+		seit Jahren an den Jahrgangslisten mitgearbeitet hat, übernommen und in den Katalog integriert.<br
+		/>
+		Neben den Ü-Ei-Inhalten sind die Fremdfiguren, zunächst eigenständig im "<span class="font-bold"
+			>FF-Kat</span
+		>" geführt, ein wichtiger Bestandteil der Katalogseiten. Themenschwerpunkte in diesem Bereich
+		sind Inhalte von Maraja, Balaban, Ravensberger und Rübezahl. Neben den Figurenserien steht auch
+		hier, insbesondere bei den deutschen Firmen, das Spielzeug mit im Vordergrund. Etwas was derzeit
+		wohl kein anderer Katalog zeigt.<br /> Die Inhalte der Kinder Joy in Deutschland und einige Berichte
+		zu unserem Hobby, wie z.B. zwei eindrucksvolle Bildberichte zum seit Ende 2002 geschlossenen Ü-Ei-Museum
+		in Dänemark, runden die Seiten ab.
+	</p>
+	<h3 class="h3 w-full mt-4">Allgemeines:</h3>
+	<p class="underline font-bold w-full mt-2">Teamwork:</p>
+	<p>
+		Diese Katalogseiten sind keine "One Man Show", sondern das Ergebnis der Zusammenarbeit vieler
+		Sammlerinnen und Sammler. Nur durch vielfältige Unterstützung ist solch ein Projekt möglich und
+		jeder kann einen zusätzlichen Beitrag leisten. Viele Informationen fehlen sicher noch im Katalog
+		und, was besonders wichtig ist, einiges an Fehlern versteckt sich noch auf den Seiten. Ich
+		brauche jede Art von Unterstützung!!
+	</p>
+	<p class="underline font-bold w-full mt-2">Bilder:</p>
+	<p>
+		Die Katalogseiten leben von den Bildern der Figuren, Aufkleber, Beipackzetteln etc. Hier brauche
+		ich auch die größte Unterstützung!! Fast jede Art von Bild ist besser als gar kein Bild aber
+		natürlich will ich letztendlich das beste Bild im Katalog zeigen. Das ständige Bestreben besser
+		zu werden und den Informationsgehalt der Seiten zu erhöhern bedeutet jedoch auch, dass ich
+		zugesandte Bilder ab und an austauschen muss. Die Kriterien hierzu sind einzig Klarheit der
+		Darstellung. Für den einzelnen Sammler, der ein Bild zugesandt hat, ist dies vielleicht
+		betrüblich, aber für das eigentliche gemeinsame Ziel: den besten Katalog im Netz zu schaffen
+		unvermeidlich.
+	</p>
+	<p class="underline font-bold w-full mt-2">Die aktuelle Saison:</p>
+	<p>
+		Die Übersicht zur aktuellen Saison steht immer zu Begin des Menüs Euro-Kat. Sie unterscheidet
+		sich im Aufbau von den Jahrgangslisten und dient dazu, erst einmal alles was an Neuem gibt zu
+		sammeln und zu veröffentlichen. Die aktuellen Serien sind dabei noch nicht in den Teilrubriken
+		oder dem alphabetischem Index erfasst.
+	</p>
+	<h3 class="h3 mt-4 w-full">Der Euro-Kat:</h3>
+	<p>
+		Hier geht es um alles, was sich in Deutschland und der EU im Überraschungsei der Fa. Ferrero
+		befunden hat. Figuren, Spielzeuge, Aufkleber, Bauanleitungen, Beipackzettel und neu die
+		Warnhinweiszettel sowie in Arbeit die Zusatzwarnzettel. Die Darstellung der Informationen
+		erfolgt auf der Grundlage von Jahrgangslisten: der Zusammenstellung von in einem Ü-Ei-Jahr (von
+		August bis Mai) erschienenen Artikel.<br /> Mit den seit 2006 in Deutschland erhältlichen
+		KinderJoy hat sich die Saison von August bis Juli verlängert. Die Jahrgangslisten Deutschland
+		und EU zeigen neben dem Bild der Figur, den BPZ, auch den BPZ, die BPZ-Rückseite und zugehörige
+		Aufkleber in übersichtlichen Datenblättern. Eine intuitive Bedienung erleichtert den Zugriff auf
+		die umfangreichen Informationen. Ist eine Serie mehrfach bzw. in unterschiedlichen Ländern
+		erschienen, werden die verschiedenen Ausgaben in einem Datenblatt dargestellt, was die
+		Unterscheidung und Zuordnung deutlich vereinfacht.<br /> Dem wachsenden Interesse an ausländischen
+		BPZ auch im Bereich der "Spielzeuge", ausgelöst zum Teil durch die Vereinheitlichung der Ü-Ei-Inhalte
+		ab 2004, Rechnung tragend, sind die D-/EU-Seiten um die Beipackzettelübersichten erweitert worden.
+		Im Bereich der Figurenserien verfügt der Euro-Kat mittlerweile über die derzeit umfangreichste Sammlung
+		deutscher und ausländischer Beipackzettel.
+	</p>
+	<h3 class="h3 w-full mt-4">Der Fremdfigurenkatalog:</h3>
+	<p>
+		Der Begriff Fremdfiguren oder auch Fremdfirmen, liebevoll "Fremdis", entstammt der Sammlerwelt
+		der Ü-Ei-Sammler (Kinder-Überraschung aus dem Hause Ferrero), welche insbesondere die
+		Figuren-Serien von Firmen wie Ravensberger (ehemals Borgmann), Onken, Bahlsen, Nestle, Eismann,
+		Bofrost, Haribo oder Zweifel als eine schöne Ergänzung ihres Sammelgebietes entdeckt haben. Auch
+		die Inhalte von dem Ü-Ei verwandten Produkten wie den Wonder-Teddys der Fa. Rübezahl (ehem.
+		Rübezahl Koch), die Ei-Inhalte der Fa. Zaini oder Schwind zählen mittlerweile hierzu.<br />Sehr
+		frei nach dem Motto "Back to the Roots" entstehen zudem vermehrt Sammelgebiete, die sich mit den
+		eigentlichen Herstellern bzw. Vertreibern dieser Produkte beschäftigen. Besonders interessant
+		sind hier für mich die Artikel der Fa. Maraja, Nuova MonPlast und Menotti.
+	</p>
+	<h3 class="h3 w-full mt-4">Spezialthemen und Berichte:</h3>
+	<p>
+		In den "Berichten" stellen wir Randbereiche oder einzelne spezielle Teilgebiete in freier Form
+		vor. Dinge die nicht in das übliche "Katalogschema" passen oder die eine besondere Fragestellung
+		behandeln. Gerade auch für diesen Bereich suche ich ständig interessante Themen und Mitarbeit.
+	</p>
+</div>
