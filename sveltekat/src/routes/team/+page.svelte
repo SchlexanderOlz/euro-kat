8ec81c9ed297328280159f4b96739a0b75a87a15
LiamKrenn <k.liam1224@gmail.com>
2023-09-07 14:15:21 +0200

    team site

---
+++ sveltekat/src/routes/team/+page.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+</script>
+
+<h1 class="h1 w-full flex justify-center mt-8 cursor-default">Das Team</h1>
+<div class="mx-[10%] mt-8 cursor-default">
+
+	<div class="card flex p-2">
+		<div class="mr-6 w-[21rem]">
+			<p class="font-bold underline mb-1.5">Helmut J.F.Henze</p>
+			<img class="w-full" src="images/helmut.jpg" />
+		</div>
+		<div class="text-sm">
+			<p class="">
+				Für alle, die mich etwas näher kennen lernen wollen. Ich bin Helmut Henze, geboren 1957 in
+				Bremen. Mittlerweile bin ich im Rentenstatus. 1997 wurde ich (wieder ;-) auf die Inhalte des
+				Überraschungsei aufmerksam. Seither beschäftige ich mich damit, alle irgendwie hierzu
+				verfügbaren Informationen zu diesem Bereich zu sammeln und anderen über meine Katalogseiten
+				zur Verfügung zu stellen. Bereits 1998 habe ich erkannt, dass auch andere Firmen
+				interessante Beigaben zu ihren Produkten legen. Von Ausnahmen abgesehen konzentriere ich
+				mich hier in der Katalogarbeit hauptsächlich auf die Hartplastikfiguren und Puzzle.
+			</p>
+			<p class="mt-4">
+				Mein besonderes Interesse gilt Informationen zu:<br />
+				Ü-Ei Inhalten Deutschland und Italien bzw. seit 2004 EU<br />
+				Fremdfiguren Deutschland,<br />
+				Schwerpunkte: Maraja und Co., Balaban, Rübezahl und Ravensberger<br />
+				PEZ-Spender, "Margarinefiguren" und Sammelbilder der 50er<br />
+				<span class="font-bold italic">Ü-Eier, Fremdfiguren</span>
+			</p>
+		</div>
+	</div>
+
+	<div class="flex mt-4">
+		<div class="card w-full mr-2 flex p-2">
+			<div class="mr-6 min-w-[12rem] w-[12rem]">
+				<p class="font-bold underline mb-1.5">Die vielen Unbekannten</p>
+				<img class="w-full" src="images/helmt.jpg" />
+			</div>
+			<div class="text-sm">
+				<p class="">
+					Ein besonderer Dank gilt den vielen unbenannten Sammlerinnen und Sammlern, die anderen und
+					auch mir über ihre Internetseiten oder durch ihre Beiträge in diversen Foren mit News und
+					Informationen rund um unser Sammelgebiet versorgt haben.<br />
+					<span class="font-bold italic">Ü-Eier, Fremdfiguren</span>
+				</p>
+			</div>
+		</div>
+		<div class="card w-full ml-2 flex p-2">
+			<div class="mr-6 min-w-[12rem] w-[12rem]">
+				<p class="font-bold underline mb-1.5">Der Fehlerteufel</p>
+				<img class="w-full" src="images/helmt.jpg" />
+			</div>
+			<div class="text-sm">
+				<p>
+					ist beständig an der Arbeit mit dem Katalog beteiligt. Mit unermüdlicher Aufopferung ist
+					er bemüht Zahlen zu verdrehen, Bilder zu verschieben und den einen oder anderen Link ins
+					irgenwo gleiten zu lassen.
+				</p>
+				<p class="mt-4">
+					Da er jedoch zu meinem langjährigsten Begleiter gehört, wäre ich für eine kurze Mail mit
+					einer Korrektur dankbar damit ich in aller Stille sein Werk verbessern kann ohne ihn zu
+					verärgern.<br />
+					<span class="font-bold italic">Ü-Eier, Fremdfiguren</span>
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="card flex p-2 mt-4">
+		<div class="mr-6 w-[18rem]">
+			<p class="font-bold underline mb-1.5">Software & Co. AG GmbH Institution</p>
+			<img class="w-full" src="images/helmt.jpg" />
+		</div>
+		<div class="text-sm">
+			<p class="">
+				Ohne der Software & Co. AG GmbH Institution wäre dieses Projekt nicht möglich gewesen.<br />
+				Wenn Sie eine Website brauchen, kontaktieren sie die Software & Co. AG GmbH Institution
+			</p>
+		</div>
+	</div>
+
+	<div class="card p-2 mt-4">
+		<p>
+			Da die Unterstützung mit Informationen und Bilder meine kühnsten Erwartungen übertroffen hat,
+			setze ich meinen Dank hierfür auf die jeweilige Einzelseite. So kann ich (hoffentlich)
+			niemanden übersehen. Jeder einzelne Beitrag ist nützlich, wichtig und des Dankes wert!
+		</p>
+	</div>
+
+    <div class="text-sm mt-8">
+        <p class="underline">Das bisherige Autorenteam des Euro Katalog bestand aus:</p>
+        <p class="font-bold mt-2">Olaf Ikels</p>
+        <p>Hartplastikfiguren, Fahrzeuge, Luftfahrt, Jahrgangslisten (Spielzeug)</p>
+        <p class="font-bold mt-2">Matthias Löwe</p>
+        <p>
+            Hartplastikfiguren, Beipackzettel, Steckfiguren, Plastikpuzzles, Bälger, Kugelfiguren,
+            Eisenbahnen, Kapselfiguren
+        </p>
+        <p class="font-bold mt-2">Manfred Schreiner</p>
+        <p>Metallfiguren</p>
+    
+        <p class=" mt-2">und bis 2001 <span class="font-bold">Conny Pay.</span></p>
+        <p class="mt-3">
+            Sie haben über Jahre hinweg das umfangreichste Informationsangebot für Sammler der Ü-Ei-Inhalte
+            im Internet zusammengetragen. Das einzigartige sind die Vielzahl von Informationen zu den
+            einzelnen Sammelobjekte. Insbesondere die reichhaltigen Informationen zur Differenzierung der
+            Figuren und Objekte zwischen ihrem Erscheinen in Deutschland und dem Ausland.
+        </p>
+    </div>
+	
+</div>
