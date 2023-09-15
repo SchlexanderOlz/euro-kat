5d3911fa087e89016f8f5b8acaca59d2c7057718
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-09-15 13:34:44 +0200

    Completed basic insert and delete queries

--- DB_Fill/TestData.ts
+++ DB_Fill/TestData.ts
@@ -39,13 +39,9 @@ async function add() {
   const data = fs.readFileSync("../html-parser/data.json", "utf8");
   const json = JSON.parse(data);
 
-  await json.forEach(async (object: Series) => {
-    let seriesData = new FormData();
-    seriesData.append("seriesLetter", object.seriesLetter);
-
-    object.subSeries.forEach(async (sub: SubSeries) => {
-      let subSeriesData = new FormData();
-      sub.figures.forEach(async (figure: Figure) => {
+  for (const object of json) {
+    for (const sub of object.subSeries) {
+      for (const figure of sub.figures) {
         let formData = new FormData();
         formData.append("mpgNr", figure.mpgNr);
         formData.append("name", figure.name);
@@ -60,22 +56,30 @@ async function add() {
           formData.append("pictures", blob, path.split("\\").pop());
         });
 
-        const record = await figures.create(formData);
-      });
+        await figures.create(formData);
+      }
+    }
+  }
 
-      sub.variations.forEach(async (variation: SubSeriesVariation) => {
+  for (const object of json) {
+    let seriesData = new FormData();
+    seriesData.append("seriesLetter", object.seriesLetter);
+
+    for (const sub of object.subSeries) {
+      let subSeriesData = new FormData();
+      for (const variation of sub.variations) {
         let subVariation = new FormData();
         subVariation.append("year", variation.year);
         subVariation.append("country", variation.country);
         subVariation.append("note", variation.note);
 
-        variation.pckgi.forEach(async (pckgi: any) => {
+        for (const pckgi of variation.pckgi) {
           let formData = new FormData();
 
           const data = fs.readFileSync(pckgi.picture);
           const blob = new Blob([data], { type: getTypeHeader(pckgi.picture) });
           formData.append("packageInserts", blob);
-          const record = await figureVariations.create(formData);
+          const figureCreate = figureVariations.create(formData);
 
           try {
             var figureRef = await figures.getFirstListItem(
@@ -83,25 +87,54 @@ async function add() {
             );
           } catch (Error) {
             console.log("Ressource not found: " + pckgi.mpgNr);
-            return;
+            continue;
           }
+          const record = await figureCreate
 
-          await figures.update(figureRef.id, {
+          figures.update(figureRef.id, {
             "figureVariations+": record.id,
           });
           subVariation.append("figureVariations", record.id);
-        });
+        }
         const record = await subSeriesVariations.create(subVariation);
         subSeriesData.append("subSeriesVariations", record.id);
-      });
+      }
 
       subSeriesData.append("name", sub.name);
       subSeriesData.append("thanks", sub.thanks);
       const record = await subSeries.create(subSeriesData);
       seriesData.append("subSeries", record.id);
-    });
+    }
     await series.create(seriesData);
+  }
+}
+
+async function dropAll(collectionName: string) {
+  await pb.admins.authWithPassword("admin@admin.admin", "Kennwort1!");
+  const collection = pb.collection(collectionName);
+  const items = await collection.getFullList();
+  for (const item of items) {
+    await collection.delete(item.id);
+  }
+}
+
+function main() {
+  const args = process.argv;
+
+  args.forEach(async (arg) => {
+    console.log(arg);
+    switch (arg) {
+      case "--drop-all":
+        dropAll("Series");
+        dropAll("Figure");
+        dropAll("FigureVariation");
+        dropAll("SubSeries");
+        dropAll("SubSeriesVariation");
+        return;
+      case "--add":
+        add();
+    }
   });
 }
 
-add();
+main();
