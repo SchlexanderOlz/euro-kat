bcebecb2e55b9eeb9b65e148ab9c568240fc0b7f
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-09-17 11:47:27 +0200

    Removed uneeded directories/files from project | Added basic queries to FigureQueryBuilder

--- sveltekat/src/lib/FigureFilter.ts
+++ sveltekat/src/lib/FigureFilter.ts
@@ -3,14 +3,8 @@ import { connection } from "./PocketBase"
 import type {
 	Series,
 	SubSeries,
-	Country,
-	Packaging,
 	Figure,
-	Variation,
-	CountryVariation,
-	PackageInsert
 } from './Types';
-
 
 class FigurFilterBuilder {
   filter: Set<string>;
@@ -21,23 +15,61 @@ class FigurFilterBuilder {
   }
 
   fake() {
-    this.filter.add("fake=true") 
+    if (this.filter.has("fake=true")) {
+      this.filter.delete("fake=true")
+      this.filter.add("fake=false")
+    } else {
+      this.filter.delete("fake=false")
+      this.filter.add("fake=true")
+    }
   }
 
   sticker() {
-    this.filter.add("sticker=true")
+    if (this.filter.has("sticker=true")) {
+      this.filter.delete("sticker=true")
+      this.filter.add("sticker=false")
+    } else {
+      this.filter.delete("sticker=false")
+      this.filter.add("sticker=true")
+    }
   }
 
-  countryVariation(country : Country) {
-    this.filter.add(`countryVariations.country=${country}`)
-  }
-
-  year(year : number){
+  yearRange(year : number){
+    // NOTE: Think about implemening this as a range-query on second-call
+    // TODO: Handle the case where there was already a range inserted before
+    for (const value of this.filter.values()) {
+      if (value.startsWith('year=')) {
+        const oldYear = Number.parseInt(value.split("=")[1])
+        this.filter.delete(value);
+        const older = Math.min(year, oldYear)
+        const younger = Math.max(year, oldYear)
+        this.filter.add(`year>=${older} && year<=${younger}`)
+        return;
+      }
+    }
     this.filter.add(`year=${year}`)
   }
 
-  mpgnumber(mpgNum: number){
-    this.filter.add(`mpgNr=${mpgNum}`)
+  year(year : number) {
+    const found = this.findRemove("year")
+    if (found) return
+    this.filter.add(`year=${year}`)
+  }
+
+  mpgnumber(mpgNr: string | undefined){
+    const found = this.findRemove("mpgNr")
+    if (found) return
+    this.filter.add(`mpgNr=${mpgNr}`)
+  }
+
+  private findRemove(search: string) {
+    for (const element of this.filter.values()) {
+      if (element.startsWith(search)) {
+        this.filter.delete(element)
+        return true
+      }
+    }
+    return false
   }
 
   async run() : Promise<Figure[]> {
--- sveltekat/src/lib/PocketBase.ts
+++ sveltekat/src/lib/PocketBase.ts
@@ -10,116 +10,18 @@ import type {
 } from './Types';
 
 export type { Series, SubSeries, SubSeriesVariation, Figure, FigureVariation};
-
-const variation: Variation = {
-	collectionId: '3',
-	collectionName: 'Variation',
-	id: '1',
-	created: 'cre',
-	updated: 'up',
-	variation: 'Has a cock',
-	picture: 'a picture of variation'
-};
-
-const countryVariation: CountryVariation = {
-	collectionId: '4',
-	collectionName: 'CountryVariation',
-	id: '1',
-	created: 'cre',
-	updated: 'up',
-	country: country,
-	year: 1999,
-	note: 'This is a variaiton'
-};
-
-const packageInserts: PackageInsert = {
-	collectionId: '5',
-	collectionName: 'PackageInsert',
-	id: '1',
-	created: 'cre',
-	updated: 'up',
-	description: 'This ís the basic package_insert',
-	picture: ['This is a picture']
-};
-
-export const figures: Figure[] = [
-	{
-		collectionId: '6',
-		collectionName: 'Figure',
-		id: '1',
-		created: 'cre',
-		updated: 'up',
-		mpgNr: 2349,
-		figureName: 'Deine mum',
-		fake: true,
-		sticker: false,
-		identifier: '1234',
-		note: 'Nice cook',
-		variation: variation,
-		pictures: ['a picture 1', 'a picture 2'],
-		countryVariations: [countryVariation],
-		packageInserts: [packageInserts, packageInserts],
-		releaseYear: 2020
-	},
-	{
-		collectionId: '6',
-		collectionName: 'Figure',
-		id: '2',
-		created: 'cre',
-		updated: 'up',
-		mpgNr: 2350,
-		figureName: 'Dein dad',
-		fake: true,
-		sticker: false,
-		identifier: '5678',
-		note: 'Nice cock',
-		variation: variation,
-		pictures: ['a picture 1', 'a picture 2'],
-		countryVariations: [countryVariation],
-		packageInserts: [packageInserts, packageInserts],
-		releaseYear: 2022
-	}
-];
-
-const subSeries: SubSeries = {
-	collectionId: '7',
-	collectionName: 'SubSeries',
-	id: '1',
-	created: 'cre',
-	updated: 'up',
-	serialName: 'Deine mum',
-	country: country,
-	thanks: 'Thank you!!!!',
-	releaseYear: 420,
-	packaging: packaging,
-	figures: figures
-};
-const series: Series = {
-	collectionId: '8',
-	collectionName: 'Series',
-	id: '1',
-	created: 'cre',
-	updated: 'up',
-	idLetters: 'mpkg',
-	year: 1420,
-	subSeries: [subSeries, subSeries]
-};
-
 export const connection: PocketBase = new PocketBase('https://ek.krenn.tech/_/');
 
 export async function getSubSeriesByLetter(seriesLetter: string): Promise<SubSeries> {
 	if (seriesLetter === 'soos') {
 		throw TypeError('Damn mann you litttle suusy bucka');
 	}
-	return subSeries;
 }
 
 export async function getSeries(): Promise<Series> {
-	return series;
 }
 
 export async function getFigureOfSeries(series: string) {
-	return figures;
 }
 
 export async function insertSeries(series: Series) {
