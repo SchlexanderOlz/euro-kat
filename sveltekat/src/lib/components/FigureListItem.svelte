08f66365415ed383ad12efc582ca404fe4fc8e89
LiamKrenn <k.liam1224@gmail.com>
2023-09-27 19:44:04 +0200

    improved responsiveness

--- sveltekat/src/lib/components/FigureListItem.svelte
+++ sveltekat/src/lib/components/FigureListItem.svelte
@@ -4,13 +4,13 @@
   export let figure: Figure;
 </script>
 
-<div class="card py-1 px-2 my-1.5 flex justify-between">
+<div class="card py-1 px-2 my-1.5 flex relative cursor-default">
   <div class="flex">
-    <p class="w-96">{figure.name}</p>
-    {#if figure.note}
-      <p class="text-surface-400">Notiz: {figure.note}</p>
+    <p class="w-80 flex-shrink-0">{figure.name}</p>
+    {#if figure.note && figure.note != 'undefined'}
+      <p class="text-surface-400 ml-4 mr-16 md:flex hidden">Notiz: {figure.note.substring(0, 30)}...</p>
     {/if}
     
   </div>
-  <p class="w-32 text-right"><span class="">Nr: </span>#{figure.mpgNr}</p>
+  <p class="w-max absolute right-2 text-right">#{figure.mpgNr}</p>
 </div>
