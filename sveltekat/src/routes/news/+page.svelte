0515bf1cdbcc31f509fc1e08f0ccb74d20c1404a
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-09-30 21:31:25 +0200

    Added missing news-templates

--- sveltekat/src/routes/news/+page.svelte
+++ sveltekat/src/routes/news/+page.svelte
@@ -24,7 +24,7 @@
 			Verfügung gestellt worden. Angesehen von kleinen "redaktionellen Änderungen (im Text kursiv)"
 			ist er im Original.
 		</p>
-		<a href="https://www.orf.at">- Link zu Bericht -</a>
+		<a href="https://pnrtscr.com">- Link zum Bericht -</a>
 	</div>
 
 	<!-- Beispiel 2 -->
@@ -42,7 +42,7 @@
 			vieles im Dunklen.
 		</p>
 		<p>.... es wird weiter dran gearbeitet!</p>
-		<a href="https://www.orf.at">- zur Bermes Liste -</a>
+		<a href="https://pnrtscr.com">- zum Bermes Liste -</a>
 	</div>
 
 	<div>
@@ -68,6 +68,7 @@
 			herausgebracht hat. Neben der Serie "Happy Family" z.B. eine 8-teilige Serie mit Miniatur
 			Dampfloks "Old Trains"
 		</p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
 	</div>
 	<div>
 		<h3>Kleine bunte Fahrzeuge (Nutzfahrzeuge)</h3>
@@ -102,7 +103,7 @@
 			Kontakt:
 			<a href="mailto:bunte-fahrzeuge@online.de">bunte-fahrzeuge@online.de</a>
 		</p>
-		<a href="KBF_2012_01_15.xls">Download der Excel-Datei (26,5 MB Stand: 15.01.2012)</a>
+		<a href="https://pnrtscr.com">- Download der Excel-Datei (26,5 MB Stand: 15.01.2012)</a>
 	</div>
 	<div>
 		<h3>Wind-Racer Spezial</h3>
@@ -111,7 +112,7 @@
 			die für reichlich Verwirrung, zumindest was mich betrifft, gesorgt hat. Im Wind-Racer Spezial
 			werden die bisher erschienen Serien und deren Unterscheidung vorgestellt.
 		</p>
-		<a href="WRspez/WRspez.htm">- zum Spezial -</a>
+		<a href="https://pnrtscr.com">- zum Spezial -</a>
 	</div>
 	<div>
 		<h3>Der Musterkoffer 1981</h3>
@@ -122,7 +123,7 @@
 			Musterkoffer für Maxi- und Ü-Ei-Inhalte. Hans-Dieter Geiger hat uns die Bilder des
 			Musterkoffers zur Ü-Ei Saison 1981-1982 zur Verfügung gestellt.
 		</p>
-		<a href="Musterk/Muster1.htm">- zum Bericht -</a>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
 	</div>
 	<div>
 		<h3>BPZ Blaudrucke</h3>
@@ -134,8 +135,126 @@
 			abgestimmt, wird hier über eine bisher unbekannte Art von Fälschungen berichtet. "BPZ
 			Blaudrucke nur für Blauäugige?"
 		</p>
-		<a href="Blaudruck/Blaudruck.htm">- zum Bericht -</a>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
 	</div>
+
+	<div>
+		<h3>Charly - Little Play Kid - Super Charly - Hej Rup</h3>
+		<p>
+        Kleine runde Knopfaugen oder leere Höhlen blicken uns entgegen und treiben der Ü-Ei Sammler an den Rand der Verzweiflung.
+        Geboren als "Little Play Kid", gewachsen zum "Super Charly", aber allgemein als "Charly" bekannt, geistert dieser kleine (ca. 5-6 cm)/ große (ca. 10-13 cm), immer lächelnde Mann durch die Spielzeug- und auch Ferrero-Geschichte.
+
+        Ob als Musiker, Matrose, Handwerker, Wikinger, Cowboy, Indianer oder in anderer Gestalt "Ferrero oder nicht Ferrero", das bleibt of die Frage. In diesem Bericht geht es um die Maraja Art.Nr. 357 und nein, hier gibt es auch nicht die erhofften Antworten.
+        Dennoch hoffe ich, dass diese Zusammenstellung nicht nur für Fremdfigurensammler von Interesse ist.
+
+        "Charly" ein Typ mit vielen Geheimnissen.
+		</p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
+	<div>
+		<h3>Magic Code / Magicode</h3>
+		<p>
+        "Magic Code" ein kleiner roter Zettel, ca. 60 x 30 mm, der in den Jahren 2001 bis 2008 den Ü-Ei Inhalten beigefügt wurde mit dem Versprechen auf jeweils 20 Minuten Spiel, Spaß und (digitale) Gewinne.
+
+        Obwohl es nur sehr wenige verschiedene Magic Code-Zettel gibt, so kann auch hier das Detail den Sammler "zum Wahnsinn treiben".
+
+        Hier eine kleine, und sicher noch nicht komplette, Anregung für alle die ............		</p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
+	<div>
+		<h3>Werbeblätter Donald Duck-Hefte Holland</h3>
+		<p>
+        Da ein Produkt für Kinder, mittlerweile auch für andere ewig jung gebliebene ;-), hat Ferrero von Beginn an Werbung in Heften und Zeitschriften für Kinder platziert. Die bekanntesten sind aus den Mickey Maus- und den Donald Duck-Heften.
+        Gerade die älteren Jahrgänge sind dabei heute wichtige Dokumentation zur Fragen: Aus dem Ei? und Wann erschienen?
+
+        In diesem Bericht werden Werbeseiten aus den Donald Duck-Heften aus Holland gezeigt. Besonders auf den ältesten Seiten sind viele Spielzeuge abgebildet. Mein persönlicher Favorit ist dabei die Ausgabe 1978 Heft 45.
+
+        Peter Brinkman stellt uns hier einige seiner Werbeblätter, darunter die Jahrgänge 1976-1980, dankenswerter Weise zur Verfügung.
+    </p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
+	<div>
+		<h3>Fantasia-Cars</h3>
+		<p>
+        Die "Fantasia Cars" erschienen in der Zeit von 1976-1993 in den Überraschungseiern Deutschland und EU.
+        Insgesamt zählen 17(18) verschiedene Fahrzeuge, die teilweise in mehreren Jahren erschienen sind, zu diesen Fahrzeugen.
+        Die Bezeichnung Fantasia Cars ist dabei erst mit den deutschen Serien aus 85/86 und 86/87 mit je 4 Fahrzeugen bestimmt. Weitere Fahrzeuge aus den früheren Jahren sind hier wegen ihrer Bauart zugeordnet.
+        Die letzte Veröffentlichung war in der Saison 1993-1994 EU in einer 6-teiligen Fahrzeugserie (deren Fahrzeuge mit der K-Kennung nach meinem Eindruck deutlich schwerer zu bekommen sind als selbst früheste Fahrzeuge von 1976).
+
+        Die Fahrzeuge sind auch im Spielwarenhandel im 6er Pack mit teilweise abweichenden Farben erhältlich.
+
+        !!! SUCHE Dringend Informationen zu den Objekten aus dem Spielzeughandel !!!
+
+        In diesem Bericht werden die verschiedenen Fahrzeuge aufgezeigt und versucht, eine zeitlichen Zuordnung der verschiedenen Varianten, sowie der korrekten Bauform (insbesondere der Kupplungen) vorzunehmen.    </p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
+	<div>
+		<h3>Das Ü-Ei Museum in Dänemark</h3>
+		<p>
+        Am 14.01.2001 eröffnete auf Farö, Dänemark das bisher weltweit einzige Ü-Ei Museum seine Pforten. Im September 2002 ergab sich für mich endlich die Gelegenheit zu einem Besuch. Sowohl was die Bandbreite der ausgestellte Objekte aus und rund um das Ü-Ei angeht, als auch die Vielzahl der Objekte in den einzelnen Sammelgebieten war sehr beeindruckend.
+        Kurz nach meinem Bsuch wurde die Ausstellung Ende 2002 leider geschlossen. Was zum einen ein etwas ungutes Gefühl über einen möglichen Zusammenhang mit meinem Besuch zurücklässt und zum anderen ein Verlust für alle Sammler darstellt. Hätte ich von der bevorstehenden Schließung gewusst, hätte ich sicher mehr und bessere Bilder mitgenommen.</p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
+	<div>
+		<h3>Le simpatiche Ranopla / Die frechen Frösche</h3>
+		<p>
+        Die Anfrage von Michela zu einer ungewöhnlichen Kennung bei den Ranopla war Anlass die Figuren noch einmal etwas genauer "unter die Lupe" zu nehmen. Im wahrsten Sinn des Wortes.
+        Das vorläufige Ergebnis zeigt, dass es sich lohnt auch anscheinend bekanntes ab und an auf seine Gültigkeit hin zu überprüfen und macht aus einer eher unscheinbaren Serie, die von Verarbeitung und Form wohl nicht zu den Glanzlichtern in der Vitrine zählt, mit einem Mal ein sehr interessantes Objekt der Betrachtung. Wer Wert darauf legt, in seiner Sammlung wirklich die richtigen Figuren zu einer Auflage zu besitzen sollten diesen Bericht unbedingt lesen.
+
+        Wer kann helfen, die unterschiedlichen Figuren ihren Erscheinungsdaten zuzuordnen? Was hat es mit der seltsamen Kennung des "umgedrehten W" von Tommy Campione auf sich? Wer kann Informationen zu den fehlenden Kennungen liefern?</p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
+	<div>
+		<h3>Le simpatiche Ranopla / Die frechen Frösche</h3>
+		<p>
+        2004 erschien die erste smart-Serie im Ü-Ei. Zunächst unter dem Hinweis einer "streng limitierten Auflage", was sich sehr schnell als Gerücht herausstellte. 2005 erschien eine weitere und 2007 gleich 2 Serien mit den kleinen "Knuddelautos".
+
+        Im smart-Spezial werden die bisher erschienen vier Serien und deren Unterscheidung vorgestellt.</p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
+	<div>
+		<h3>Das smart Spezial</h3>
+		<p>
+        2004 erschien die erste smart-Serie im Ü-Ei. Zunächst unter dem Hinweis einer "streng limitierten Auflage", was sich sehr schnell als Gerücht herausstellte. 2005 erschien eine weitere und 2007 gleich 2 Serien mit den kleinen "Knuddelautos".
+
+        Im smart-Spezial werden die bisher erschienen vier Serien und deren Unterscheidung vorgestellt.</p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
+	<div>
+		<h3>Blumenzauber</h3>
+		<p>
+    Insgesamt vier mal sind die Figuren der Serie "Blumenzauber" bisher in der EU bzw. in Deutschland erschienen. Zur Unterscheidung der Figuren zwischen den einzelnen Auflagen haben wir diesen Bericht erstellt.
+    </p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
+	<div>
+		<h3>Das Magic Sport 2 Spezial</h3>
+		<p>
+      Bei der Serie Magic Sport 2 bedarf es einer UV-Lampe um die Unterschiede zu erkennen und das Zubehör den Figuren zweifelsfrei zuzuordnen.
+
+      Im Magic Sport 2-Spezial werden die Unterschiede und die tatsächlich im Ei gewesenen Kombinationen aufgezeigt.
+    </p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
+	<div>
+		<h3>Das Ü-Ei Museum in Dänemark die 2. </h3>
+		<p>
+        Nach dem 1. Bildbericht über das Ü-Ei Museum auf Farö, Dänemark hat uns nun auch Sigrid mit einer Bildserie überrascht.
+        Es ist mir eine besondere Freude diese Bilder hier zeigen zu dürfen.
+    </p>
+		<a href="https://pnrtscr.com">- zum Bericht -</a>
+	</div>
+
 </div>
 
 <!-- Standard-Bericht Styles (nicht verändern, außer man weiß, was man tut!) -->
