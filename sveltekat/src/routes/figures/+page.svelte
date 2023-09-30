23dd466fd326f3272d5099536d832e07140293ca
LiamKrenn <k.liam1224@gmail.com>
2023-09-30 11:01:54 +0200

    aliiii

--- sveltekat/src/routes/figures/+page.svelte
+++ sveltekat/src/routes/figures/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import type { Figure } from '$lib/PocketBase';
+	import { connection, getFigureDetail, type Figure } from '$lib/PocketBase';
+	import type { FigurePage } from '$lib/Types';
 	import FigureList from '$lib/components/FigureList.svelte';
 	import FigureListItem from '$lib/components/FigureListItem.svelte';
 	import type { PageData } from './$types';
@@ -9,6 +10,14 @@
 	const figures: Figure[] = data.figures;
 
 	//implement load more
+
+	let res: FigurePage;
+	async function test()  {
+	res = await getFigureDetail("b04vbp0wtedmfqj")
+	//console.log(res);
+	}
+	test()
+	
 </script>
 
 <div class="h-full w-full sm:px-[10%] px-[5%] sm:mt-12 mt-6 mb-16">
