7b5a5b232c9ff419ff7bf8e6b26d6a52390bec51
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-09-30 14:51:21 +0200

    Added optional queries in FigureBuilder

--- sveltekat/src/lib/FigureFilter.ts
+++ sveltekat/src/lib/FigureFilter.ts
@@ -4,20 +4,24 @@ import type { Series, SubSeries, Figure } from './Types';
 
 export class FigurFilterBuilder {
 	filter: Set<string>;
-  expand: string;
 	figureCollection: RecordService<Figure>;
+	optionals: Set<string>;
 
 	constructor() {
 		this.figureCollection = connection.collection('Figure');
-    this.expand = "";
+		this.optionals = new Set();
 		this.filter = new Set();
 	}
 
 	fake() {
-    this.toggleBoolConatined("fake")
+		this.toggleBoolConatined('fake');
 	}
 
-  private toggleBoolConatined(field: string) {
+	killFake() {
+		this.findRemove("fake", this.filter)
+	}
+
+	private toggleBoolConatined(field: string) {
 		if (this.filter.has(`${field}=true`)) {
 			this.filter.delete(`${field}=true`);
 			this.filter.add(`${field}=false`);
@@ -25,70 +29,74 @@ export class FigurFilterBuilder {
 			this.filter.delete(`${field}=false`);
 			this.filter.add(`${field}=true`);
 		}
-  }
+	}
 
 	questionable() {
-    this.toggleBoolConatined("questionable")
+		this.toggleBoolConatined('questionable');
+	}
+
+	killQuestionable() {
+		this.findRemove("questionable", this.filter)
 	}
 
 	sticker() {
-    this.toggleBoolConatined("sticker")
+		this.toggleBoolConatined('sticker');
 	}
 
-  yearBegin(year: number | undefined) {
-    this.findRemove('year>=')
-    this.filter.add(`year>=${year}`)
-  }
+	killSticker() {
+		this.findRemove("sticker", this.filter)
+	}
 
-  yearEnd(year: number | undefined) {
-    this.findRemove('year<=')
-    this.filter.add(`year<=${year}`)
-  }
+	yearBegin(year: number | undefined) {
+		this.findRemove('year>=', this.filter);
+		this.filter.add(`year>="${year}"`);
+	}
+
+	yearEnd(year: number | undefined) {
+		this.findRemove('year<=', this.filter);
+		this.filter.add(`year<="${year}"`);
+	}
 
 	year(year: number | undefined) {
-		const found = this.findRemove('year=');
-		if (found) return;
-		this.filter.add(`year=${year}`);
+		this.findRemove('year=', this.filter);
+		if (year == undefined) return;
+		this.filter.add(`year="${year}"`);
 	}
 
-  name(name: string) {
-    this.findRemove("name ~")
-    this.filter.add(`name ~ "${name}"`)
-  }
+	name(name: string) {
+		this.findRemove('name~', this.optionals);
+		this.optionals.add(`name~"${name}"`);
+	}
 
-  identifier(id: string) {
-    this.findRemove("identifier~")
-    this.filter.add(`identifier~ "${id}"`)
-  }
+	identifier(id: string) {
+		this.findRemove('identifier~', this.optionals);
+		this.optionals.add(`identifier~"${id}"`);
+	}
 
-  note(note: string) {
-    this.findRemove("note~")
-    this.filter.add(`note~${note}`)
-  }
+	note(note: string) {
+		this.findRemove('note~', this.optionals);
+		this.optionals.add(`note~"${note}"`);
+	}
 
-  id(id: string) {
-    this.findRemove("id=")
-    this.filter.add(`id=${id}`)
-  }
+	id(id: string) {
+		this.findRemove('id=', this.filter);
+		this.filter.add(`id="${id}"`);
+	}
 
 	mpgnumber(mpgNr: string | undefined) {
-		this.findRemove('mpgNr ~');
-		this.filter.add(`mpgNr ~ "${mpgNr}"`);
+		this.findRemove('mpgNr~', this.optionals);
+		this.optionals.add(`mpgNr~"${mpgNr}"`);
 	}
 
-  country(country: string) {
-    if (!(this.expand == "FigureVariation,SubSeriesVariation(figureVariation).country")) {
-      this.expand = "FigureVariation,SubSeriesVariation(figureVariation).country"
-    }
+	country(country: string) {
+		this.findRemove('country=', this.filter);
+		this.filter.add(`country="${country}"`);
+	}
 
-    this.findRemove("country=")
-    this.filter.add(`country=${country}`)
-  }
-
-	private findRemove(search: string) {
-		for (const element of this.filter.values()) {
+	private findRemove(search: string, lookupList: Set<string>) {
+		for (const element of lookupList.values()) {
 			if (element.startsWith(search)) {
-				this.filter.delete(element);
+				lookupList.delete(element);
 				return true;
 			}
 		}
@@ -96,10 +104,28 @@ export class FigurFilterBuilder {
 	}
 
 	async run(): Promise<Figure[]> {
-		// Remove this implicit copy -> f.e. manull iteration
+		if (this.filter.size == 0 && this.optionals.size == 0) return await this.figureCollection.getList(1, figureInitLoadCount);
+
+		let query = "";
+		if (this.filter.size > 0) {
+			query = Array.from(this.filter).join("&&")
+			if (this.optionals.size > 0) query += "&&("
+		}
+		if (this.optionals.size > 0) {
+			query += Array.from(this.optionals).join("||")
+			if (this.filter.size > 0) {
+				query += ")"
+			}
+		}
+
 		return structuredClone(
 			// TODO: How to handle multiple pages
-			(await this.figureCollection.getList(1, figureInitLoadCount, { filter: Array.from(this.filter).join(' && '), expand: this.expand })).items
+			(
+				await this.figureCollection.getList(1, figureInitLoadCount, {
+					filter: query,
+					expand: "subSeriesId"
+				})
+			).items
 		);
 	}
 }
