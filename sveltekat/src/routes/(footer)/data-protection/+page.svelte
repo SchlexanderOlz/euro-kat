7ab114b4dcdf954708f14f2d1c176c5fcfd688fd
LiamKrenn <k.liam1224@gmail.com>
2023-10-01 00:18:13 +0200

    updated data protection

--- sveltekat/src/routes/(footer)/data-protection/+page.svelte
+++ sveltekat/src/routes/(footer)/data-protection/+page.svelte
@@ -5,166 +5,97 @@
 	<h1 class="justify-center w-full flex h1 mt-6 mb-8 break-all text-center">
 		Datenschutzerklärung
 	</h1>
-	<p class="text-red-600 text-3xl w-full flex justify-center font-bold">Bitte drüber lesen</p>
+
 	<p>
-		Personenbezogene Daten (nachfolgend zumeist nur „Daten“ genannt) werden von mir (Anbieter) durch
-		die Nutzung des Internetauftritts, inklusive seiner Inhalte und der dort angebotenen Leistungen
-		nicht erhoben und demnach auch nicht verarbeitet.
+		Wir nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen
+		Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser
+		Datenschutzerklärung.
+		<br /><br />
+		Die Nutzung unserer Website ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit
+		auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder E-Mail-Adressen) erhoben
+		werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre
+		ausdrückliche Zustimmung nicht an Dritte weitergegeben.
+		<br /><br />
+		Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail)
+		Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist
+		nicht möglich.
 	</p>
+
+	<h2>Cookies</h2>
 	<p>
-		Gemäß Art. 4 Ziffer 1. der Verordnung (EU) 2016/679, also der Datenschutz-Grundverordnung
-		(nachfolgend nur „DSGVO“ genannt), gilt als „Verarbeitung“ jeder mit oder ohne Hilfe
-		automatisierter Verfahren ausgeführter Vorgang oder jede solche Vorgangsreihe im Zusammenhang
-		mit personenbezogenen Daten, wie das Erheben, das Erfassen, die Organisation, das Ordnen, die
-		Speicherung, die Anpassung oder Veränderung, das Auslesen, das Abfragen, die Verwendung, die
-		Offenlegung durch Übermittlung, Verbreitung oder eine andere Form der Bereitstellung, den
-		Abgleich oder die Verknüpfung, die Einschränkung, das Löschen oder die Vernichtung.
+		Die Internetseiten verwenden teilweise so genannte Cookies. Cookies richten auf Ihrem Rechner
+		keinen Schaden an und enthalten keine Viren. Cookies dienen dazu, unser Angebot
+		nutzerfreundlicher, effektiver und sicherer zu machen. Cookies sind kleine Textdateien, die auf
+		Ihrem Rechner abgelegt werden und die Ihr Browser speichert.
+		<br /><br />
+		Wir verwenden Cookies, um die Funktionalität unserer Website zu gewährleisten und um das Nutzererlebnis
+		zu verbessern. Cookies sind kleine Textdateien, die auf Ihrem Computer oder Mobilgerät gespeichert
+		werden, wenn Sie unsere Website besuchen.
+		<br /><br />
+		Wir verwenden Cookies, um sicherzustellen, dass unsere Website ordnungsgemäß funktioniert, um Ihre
+		Einstellungen zu speichern und um anonyme Statistiken über die Nutzung unserer Website zu sammeln.
+		Wir verwenden keine Cookies, um personenbezogene Daten zu sammeln oder um Werbung anzuzeigen.
+		<br /><br />
+		Sie können die Verwendung von Cookies in Ihrem Browser deaktivieren oder einschränken. Bitte beachten
+		Sie jedoch, dass einige Funktionen unserer Website möglicherweise nicht ordnungsgemäß funktionieren,
+		wenn Sie die Verwendung von Cookies deaktivieren.
+		<br /><br />
+		Indem Sie unsere Website weiterhin nutzen, stimmen Sie der Verwendung von Cookies gemäß dieser Cookie-Richtlinie
+		zu.
 	</p>
+
+	<h2>Server-Log-Files</h2>
+	Der Provider der Seiten erhebt und speichert automatisch Informationen in so genannten Server-Log Files,
+	die Ihr Browser automatisch an uns übermittelt. Dies sind:
+	<br /><br />
+	- Browsertyp und Browserversion<br />
+	- verwendetes Betriebssystem<br />
+	- Referrer URL<br />
+	- Hostname des zugreifenden Rechners<br />
+	- Uhrzeit der Serveranfrage<br />
+	<br /><br />
+	Diese Daten sind nicht bestimmten Personen zuordenbar. Eine Zusammenführung dieser Daten mit anderen
+	Datenquellen wird nicht vorgenommen. Wir behalten uns vor, diese Daten nachträglich zu prüfen, wenn
+	uns konkrete Anhaltspunkte für eine rechtswidrige Nutzung bekannt werden.
+
+	<h2>Cloudflare</h2>
 	<p>
-		Mit der nachfolgenden Datenschutzerklärung informiere ich Sie insbesondere über Art, Umfang,
-		Zweck, Dauer und Rechtsgrundlage der Verarbeitung personenbezogener Daten, soweit ich entweder
-		allein oder gemeinsam mit anderen über die Zwecke und Mittel der Verarbeitung entscheide. Zudem
-		informiere ich Sie nachfolgend über die von mir zu Optimierungszwecken sowie zur Steigerung der
-		Nutzungsqualität eingesetzten Fremdkomponenten, soweit hierdurch Dritte Daten in wiederum
-		eigener Verantwortung verarbeiten.
+		Unsere Website nutzt Funktionen von Cloudflare als Sicherheitsmaßnahme. Anbieter ist die
+		Cloudflare Inc., 101 Townsend St, San Francisco, CA 94107, USA.
+		<br /><br />
+		Cloudflare bietet ein weltweit verteiltes Content Delivery Network mit DNS an. Technisch wird der
+		Informationstransfer zwischen Ihrem Browser und unserer Website über das Netzwerk von Cloudflare
+		geleitet. Cloudflare ist damit in der Lage, den Datenverkehr zwischen Benutzern und unserer Website
+		zu analysieren, um beispielsweise Angriffe auf unsere Dienste zu erkennen und abzuwehren. Zudem speichert
+		Cloudflare unter Umständen bestimmte Informationen, um die Sicherheit und Performance des Netzwerks
+		zu optimieren.
+		<br /><br />
+		Die Nutzung von Cloudflare erfolgt im Interesse einer sicheren Bereitstellung unserer Online-Angebote.
+		Dies stellt ein berechtigtes Interesse im Sinne von Art. 6 Abs. 1 lit. f DSGVO dar.
+		<br /><br />
+		Weitere Informationen finden Sie in der Datenschutzerklärung von Cloudflare:
+		<a href="https://www.cloudflare.com/privacypolicy/">Cloudflare Datenschutzerklärung.</a>
 	</p>
-	<h3>I. Informationen über mich als Verantwortlichen</h3>
-	<p>Siehe <a href="/imprint">Impressum</a></p>
-	<h3>II. Rechte der Nutzer und Betroffenen</h3>
+
+	<h2>Vercel</h2>
 	<p>
-		Mit Blick auf die nachfolgend noch näher beschriebene Datenverarbeitung haben die Nutzer und
-		Betroffenen das Recht
-	</p>
-	<ul>
-		<li>
-			auf Bestätigung, ob sie betreffende Daten verarbeitet werden, auf Auskunft über die
-			verarbeiteten Daten, auf weitere Informationen über die Datenverarbeitung sowie auf Kopien der
-			Daten (vgl. auch Art. 15 DSGVO);
-		</li>
-		<li>
-			auf Berichtigung oder Vervollständigung unrichtiger bzw. unvollständiger Daten (vgl. auch Art.
-			16 DSGVO);
-		</li>
-		<li>
-			auf unverzügliche Löschung der sie betreffenden Daten (vgl. auch Art. 17 DSGVO), oder,
-			alternativ, soweit eine weitere Verarbeitung gemäß Art. 17 Abs. 3 DSGVO erforderlich ist, auf
-			Einschränkung der Verarbeitung nach Maßgabe von Art. 18 DSGVO;
-		</li>
-		<li>
-			auf Erhalt der sie betreffenden und von ihnen bereitgestellten Daten und auf Übermittlung
-			dieser Daten an andere Anbieter/Verantwortliche (vgl. auch Art. 20 DSGVO);
-		</li>
-		<li>
-			auf Beschwerde gegenüber der Aufsichtsbehörde, sofern sie der Ansicht sind, dass die sie
-			betreffenden Daten durch den Anbieter unter Verstoß gegen datenschutzrechtliche Bestimmungen
-			verarbeitet werden (vgl. auch Art. 77 DSGVO).
-		</li>
-	</ul>
-	<p>
-		Darüber hinaus ist der Anbieter dazu verpflichtet, alle Empfänger, denen gegenüber Daten durch
-		den Anbieter offengelegt worden sind, über jedwede Berichtigung oder Löschung von Daten oder die
-		Einschränkung der Verarbeitung, die aufgrund der Artikel 16, 17 Abs. 1, 18 DSGVO erfolgt, zu
-		unterrichten. Diese Verpflichtung besteht jedoch nicht, soweit diese Mitteilung unmöglich oder
-		mit einem unverhältnismäßigen Aufwand verbunden ist. Unbeschadet dessen hat der Nutzer ein Recht
-		auf Auskunft über diese Empfänger.
-	</p>
-	<p class="inhaltdick">
-		Ebenfalls haben die Nutzer und Betroffenen nach Art. 21 DSGVO das Recht auf Widerspruch gegen
-		die künftige Verarbeitung der sie betreffenden Daten, sofern die Daten durch den Anbieter nach
-		Maßgabe von Art. 6 Abs. 1 lit. f) DSGVO verarbeitet werden.
-	</p>
-	<h3>III. Informationen zur Datenverarbeitung</h3>
-	<p>Durch die Nutzung meiner Internetauftritte werden keine personenbezogenen Daten erhoben.</p>
-	<p>
-		Die Sammlerseiten leben von der Unterstützung durch Sammlerinnen und Sammler aus der ganzen
-		Welt.<br />Es ist mir wichtig, mich für jede zugesendete Information, jedes zugesendete Bild zu
-		bedanken, indem ich Name und Vorname der Zusenderin/des Zusenders auf der jeweiligen Seite
-		angebe. Diese Angabe dient gleichzeitig dem Nachweis über die Bildherkunft. Dieses wende ich
-		jetzt über 10 Jahre an und ist für alle Besucherinnen und Besucher meiner Seiten sichtbar.
-	</p>
-	<p class="inhaltdick">
-		Mit der Zusendung einer Information, eines Bildes erklären Sie sich mit der Veröffentlich Ihres
-		Namen und Ihres Vornamens einverstanden! Sollten Sie dieses nicht wünschen, so teilen Sie mir
-		dieses in der Mail mit
-	</p>
-	<p class="inhaltdick">Serverdaten</p>
-	<p>
-		Aus technischen Gründen, insbesondere zur Gewährleistung eines sicheren und stabilen
-		Internetauftritts, werden Daten durch Ihren Internet-Browser an uns bzw. an unseren
-		Webspace-Provider übermittelt. Mit diesen sog. Server-Logfiles werden u.a. Typ und Version Ihres
-		Internetbrowsers, das Betriebssystem, die Website, von der aus Sie auf unseren Internetauftritt
-		gewechselt haben (Referrer URL), die Website(s) unseres Internetauftritts, die Sie besuchen,
-		Datum und Uhrzeit des jeweiligen Zugriffs sowie die IP-Adresse des Internetanschlusses, von dem
-		aus die Nutzung unseres Internetauftritts erfolgt, erhoben.
-	</p>
-	<p>
-		Diese so erhobenen Daten werden vorrübergehend gespeichert, dies jedoch nicht gemeinsam mit
-		anderen Daten von Ihnen.
-	</p>
-	<p>
-		Diese Speicherung erfolgt auf der Rechtsgrundlage von Art. 6 Abs. 1 lit. f) DSGVO. Unser
-		berechtigtes Interesse liegt in der Verbesserung, Stabilität, Funktionalität und Sicherheit
-		unseres Internetauftritts.
-	</p>
-	<p>
-		Die Daten werden spätestens nach sieben Tage wieder gelöscht, soweit keine weitere Aufbewahrung
-		zu Beweiszwecken erforderlich ist. Andernfalls sind die Daten bis zur endgültigen Klärung eines
-		Vorfalls ganz oder teilweise von der Löschung ausgenommen.
-	</p>
-	<h3>Haftungsausschluss</h3>
-	<p class="inhaltdick">1. Inhalt des Onlineangebotes</p>
-	<p>
-		Der Autor übernimmt keinerlei Gewähr für die Aktualität, Korrektheit, Vollständigkeit oder
-		Qualität der bereitgestellten Informationen. Haftungsansprüche gegen den Autor, welche sich auf
-		Schäden materieller oder ideeller Art beziehen, die durch die Nutzung oder Nichtnutzung der
-		dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und unvollständiger Informationen
-		verursacht wurden, sind grundsätzlich ausgeschlossen, sofern seitens des Autors kein
-		nachweislich vorsätzliches oder grob fahrlässiges Verschulden vorliegt.<br />Alle Angebote sind
-		freibleibend und unverbindlich. Der Autor behält es sich ausdrücklich vor, Teile der Seiten oder
-		das gesamte Angebot ohne gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die
-		Veröffentlichung zeitweise oder endgültig einzustellen.
-	</p>
-	<p class="inhaltdick">2. Verweise und Links</p>
-	<p>
-		Bei direkten oder indirekten Verweisen auf fremde Webseiten ("Hyperlinks"), die außerhalb des
-		Verantwortungsbereiches des Autors liegen, würde eine Haftungsverpflichtung ausschließlich in
-		dem Fall in Kraft treten, in dem der Autor von den Inhalten Kenntnis hat und es ihm technisch
-		möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger Inhalte zu verhindern.<br />Der
-		Autor erklärt hiermit ausdrücklich, dass zum Zeitpunkt der Linksetzung keine illegalen Inhalte
-		auf den zu verlinkenden Seiten erkennbar waren. Auf die aktuelle und zukünftige Gestaltung, die
-		Inhalte oder die Urheberschaft der verlinkten/verknüpften Seiten hat der Autor keinerlei
-		Einfluss. Deshalb distanziert er sich hiermit ausdrücklich von allen Inhalten aller verlinkten
-		/verknüpften Seiten sowie den Inhalten der von diesen Seiten weiterführenden Verlinkungen, die
-		nach der Linksetzung verändert wurden. Diese Feststellung gilt für alle innerhalb des eigenen
-		Internetangebotes gesetzten Links und Verweise sowie für Fremdeinträge in vom Autor
-		eingerichteten Gästebüchern, Diskussionsforen, Linkverzeichnissen, Mailinglisten und in allen
-		anderen Formen von Datenbanken, auf deren Inhalt externe Schreibzugriffe möglich sind. Für
-		illegale, fehlerhafte oder unvollständige Inhalte und insbesondere für Schäden, die aus der
-		Nutzung oder Nichtnutzung solcherart dargebotener Informationen entstehen, haftet allein der
-		Anbieter der Seite, auf welche verwiesen wurde, nicht derjenige, der über Links auf die
-		jeweilige Veröffentlichung lediglich verweist.
-	</p>
-	<p class="inhaltdick">3. Urheber- und Kennzeichenrecht</p>
-	<p>
-		Der Autor ist bestrebt, in allen Publikationen die Urheberrechte der verwendeten Grafiken,
-		Tondokumente, Videosequenzen und Texte zu beachten, von ihm selbst erstellte Grafiken,
-		Tondokumente, Videosequenzen und Texte zu nutzen oder auf lizenzfreie Grafiken, Tondokumente,
-		Videosequenzen und Texte zurückzugreifen.<br />Alle innerhalb des Internetangebotes genannten
-		und ggf. durch Dritte geschützten Marken- und Warenzeichen unterliegen uneingeschränkt den
-		Bestimmungen des jeweils gültigen Kennzeichenrechts und den Besitzrechten der jeweiligen
-		eingetragenen Eigentümer. Allein aufgrund der bloßen Nennung ist nicht der Schluss zu ziehen,
-		dass Markenzeichen nicht durch Rechte Dritter geschützt sind!<br />Das Copyright für
-		veröffentlichte, vom Autor selbst erstellte Objekte bleibt allein beim Autor der Seiten. Eine
-		Vervielfältigung oder Verwendung solcher Grafiken, Tondokumente, Videosequenzen und Texte in
-		anderen elektronischen oder gedruckten Publikationen ist ohne ausdrückliche Zustimmung des
-		Autors nicht gestattet.
-	</p>
-	<p class="inhaltdick">4. Rechtswirksamkeit dieses Haftungsausschlusses</p>
-	<p>
-		Dieser Haftungsausschluss ist als Teil des Internetangebotes zu betrachten, von dem aus auf
-		diese Seite verwiesen wurde. Sofern Teile oder einzelne Formulierungen dieses Textes der
-		geltenden Rechtslage nicht, nicht mehr oder nicht vollständig entsprechen sollten, bleiben die
-		übrigen Teile des Dokumentes in ihrem Inhalt und ihrer Gültigkeit davon unberührt.
+		Unsere Website nutzt Funktionen von Vercel als Content Delivery Network (CDN). Anbieter ist die
+		Vercel Inc., 340 S Lemon Ave #4133 Walnut, CA 91789, USA.
+		<br /><br />
+		Vercel bietet ein weltweit verteiltes Content Delivery Network an, das es uns ermöglicht, unsere
+		Website schnell und zuverlässig bereitzustellen. Technisch wird der Informationstransfer zwischen
+		Ihrem Browser und unserer Website über das Netzwerk von Vercel geleitet. Vercel ist damit in der
+		Lage, den Datenverkehr zwischen Benutzern und unserer Website zu analysieren, um beispielsweise die
+		Performance und Sicherheit des Netzwerks zu optimieren.
+		<br /><br />
+		Die Nutzung von Vercel erfolgt im Interesse einer schnellen und zuverlässigen Bereitstellung unserer
+		Online-Angebote. Dies stellt ein berechtigtes Interesse im Sinne von Art. 6 Abs. 1 lit. f DSGVO dar.
+		<br /><br />
+		Weitere Informationen finden Sie in der Datenschutzerklärung von Vercel:
+		<a href="https://vercel.com/legal/privacy-policy">Vercel Datenschutzerklärung</a>
+		und <a href="https://vercel.com/docs/concepts/analytics/privacy-policy"
+			>Vercel Analytics Privacy and Compliance</a
+		>.
 	</p>
 </div>
 
@@ -175,19 +106,7 @@
 	a {
 		@apply underline;
 	}
-	h3 {
+	h2 {
 		@apply h3 mt-8 mb-2;
-	}
-	p {
-		@apply mb-2;
-	}
-	p.inhaltdick {
-		@apply font-bold;
-	}
-	ul {
-		@apply mb-2 mt-1;
-	}
-	li {
-		@apply list-disc ml-12 mb-1;
 	}
 </style>
