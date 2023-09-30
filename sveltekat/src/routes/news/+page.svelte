e0f949c1105b540b284146fb5104923a095a297b
LeitnerLukas <91020705+LeitnerLukas@users.noreply.github.com>
2023-09-30 19:01:58 +0200

    first linked reports of original euro-kat

--- sveltekat/src/routes/news/+page.svelte
+++ sveltekat/src/routes/news/+page.svelte
@@ -51,6 +51,99 @@
 
 	<!-- Restliche Berichte von Euro-Kat Berichte -->
 	<p>Restliche Berichte hinzufügen</p>
+
+	<div>
+		<h3>Happy Family</h3>
+		<p>
+			Die verschiedenen "Babys mit Zubehör" (Ende der 70er bis Anfang 80er im Ü-Ei) beinhalten für mich persönlich noch eine ganze Reihe von offenen Fragen.
+			RP-Kennung der Babys durchgängig "Ja" oder "sowohl als auch"? Babys mit Haushaltsgegenständen in rot?; Babyzubehör (Schaukel, Wiege, Kinderwagen, Buggy etc.) mit oder ohne Baby im Ei?
+			Gab es die Serien/Teile in gleicher Form auch im Spielzeughandel?
+		</p>
+		<p>
+			Die 8-teilge Serie "Happy Family" der italienischen Firma KLIC (erschienen vermutlich in den 70er Jahren)
+			beantwortet zumindest die letzte Frage mit einem klaren JA!
+		</p>
+		<p>
+			Erwähnenswert vielleicht noch, dass, obwohl diese Serie ja ebenfalls bereits ein beträchtliches Alter erreicht hat, diese Artikel in größerer Zahl immer noch angeboten werden.
+		</p>
+		<p>
+			Leider ist es mir bis jetzt nicht gelungen nähere Informationen zur Firma KLIC zu finden.
+			Bekannt ist mir nur, dass die Firma KLIC in den 70er Jahren mehrere Miniatur Plastik Serien herausgebracht hat.
+			Neben der Serie "Happy Family" z.B. eine 8-teilige Serie mit Miniatur Dampfloks "Old Trains"
+		</p>
+	</div>
+	<div>
+		<h3>Kleine bunte Fahrzeuge (Nutzfahrzeuge)</h3>
+		<p>
+			Anfang Januar 2012 erreichte mich folgende email:
+					Hallo Helmut,
+					Als Anlage ein Sammelgebiet das durch das Sammeln von Ü Eiern und deren Inhalten entstehen kann. Bei mir ist dieses vor einiger Zeit passiert.
+					Von überall und nirgends kommen Tüten voller Ü Ei " Figuren " an. Da ich ein Jahrgangssammler bin habe ich die Tüten + Kisten entsprechend durchforstet und sortiert. Ich finde aus den Eiern auch ganz speziell die Fahrzeuge und Autos super.
+
+					Irgendwann hatte ich eine Kiste voll Bunter Fahrzeuge die nicht aus den Eiern sind. Aber jeder der seinen Ü Ei Kram zusammensammelt und in eine Tüte schmeißt packt die BUNTEN FAHRZEUGE mit ein.
+					Daraus ist die angehängte Datei entstanden.
+					Die Fahrzeuge stammen aus Wundertüten oder Joghurts oder Ü Ei oder oder oder ..............
+					Hersteller Bruder, TM oder Nahost oder .......
+
+					Viel Spaß damit und mach damit was du möchtest.
+					Gruß Peter Wirth aus Berlin
+		</p>
+		<p>
+			Peter Wirth hat eine wirklich beeindruckende Übersicht der verschiedenen Fahrzeuge,
+			deren Merkmale und Unterschiede geschaffen. 
+			Besonders interessant wird sie auch durch den Katalogteil,
+			der zumindest die Quelle für einen Großteil der Fahrzeuge erschließt.
+		</p>
+		<p>
+			Gutes kann man nicht besser machen,
+			einmal abgesehen von der Mordsarbeit die damit verbunden wäre solchen immensen Informationsumfang umzusetzen ;-),
+			also steht die Originaltabelle allen Interessierten zum Download bereit.
+		</p>
+		<p>
+			Meinen herzlichen Dank an Peter Wirth für diese Arbeit und das zur Verfügung stellen!
+		</p>
+		<p>
+			Sicher ist Peter Wirth für jede Art von Anregung, Ergänzung oder sonstigem "Feed-back" dankbar.
+		</p>
+		<p>Kontakt:
+		<a href="mailto:bunte-fahrzeuge@online.de">
+		bunte-fahrzeuge@online.de</a></p>
+		<a href="KBF_2012_01_15.xls">Download der Excel-Datei (26,5 MB Stand: 
+		15.01.2012)</a>
+	</div>
+	<div>
+		<h3>Wind-Racer Spezial</h3>
+		<p>
+			2010 erschienen die ersten zwei Wind-Racer Serie im Kinder Joy. 2011 erschien "eine" weitere, die für reichlich Verwirrung, zumindest was mich betrifft, gesorgt hat.
+
+			Im Wind-Racer Spezial werden die bisher erschienen Serien und deren Unterscheidung vorgestellt.
+		</p>
+		<a href="WRspez/WRspez.htm">- zum Spezial -</a>
+	</div>
+	<div>
+		<h3>Der Musterkoffer 1981</h3>
+		<p>
+			Werbeblätter und sonstiges Info-Material (z.B. Zeitschriftenwerbung) von Ferrero sind häufig das einzige Mittel um neben dem "Ob" auch das "Wann", gerade für alles was nicht Hartplastik ist, zu bestimmen.
+			Ein besonderes Werbemittel der Ferrero-Vertreter waren 1980 die Musterkoffer für Maxi- und Ü-Ei-Inhalte.
+			
+			Hans-Dieter Geiger hat uns die Bilder des Musterkoffers zur Ü-Ei Saison 1981-1982 zur Verfügung gestellt. 
+		</p>
+		<a href="Musterk/Muster1.htm">- zum Bericht -</a>
+	</div>
+	<div>
+		<h3>BPZ Blaudrucke</h3>
+		<p>
+			BPZ Blaudrucke gehören mit zu den begehrtesten Objekten für Figuren und BPZ Sammler. Preise zwischen 50 und 100€ sind ob der Seltenheit dieser Sammelstücke durchaus im üblichen Rahmen.
+
+			Bevor sich beim nächsten Schnäppchenangebot aber die Brieftasche öffnet bitte zunächst diesen Bericht lesen!!
+
+			Gewissenhaft und sorgfältig recherchiert, mit renommierten Sammlern abgestimmt, wird hier über eine bisher unbekannte Art von Fälschungen berichtet.
+
+			"BPZ Blaudrucke nur für Blauäugige?"
+		</p>
+		<a href="Blaudruck/Blaudruck.htm">- zum Bericht -</a>
+	</div>
+
 </div>
 
 <!-- Standard-Bericht Styles (nicht verändern, außer man weiß, was man tut!) -->
