d75b1d65cab0ec3eeb2af42a4252bfac6087380a
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2024-04-19 18:48:55 +0200

    Fixed double-display of marked figures | Added only series-name to mobile display

--- sveltekat/src/lib/components/FigureListItem.svelte
+++ sveltekat/src/lib/components/FigureListItem.svelte
@@ -13,48 +13,12 @@
 	href="/figures/{figure.id}"
 >
 	<div class="flex">
-		<p class="sm:w-80 w-full sm:mr-0 mr-2 overflow-hidden sm:flex-shrink-0 break-all">{figure.name}</p>
+		<p class="sm:w-80 w-full sm:mr-0 mr-2 overflow-hidden sm:flex-shrink-0 break-all md:flex hidden">{figure.name}</p>
 		{#if figure.expand.subSeriesId?.name }
-			<p class="text-surface-400 ml-4 mr-16 md:flex hidden overflow-y-hidden break-all">
+			<p class="md:text-surface-400 md:ml-4 mr-16 md:flex overflow-y-hidden break-all">
 				{figure.expand.subSeriesId.name.substring(0, 30)}...
 			</p>
 		{/if}
 	</div>
 	<p class="w-max right-2 text-right flex-shrink-0">#{figure.mpgNr}</p>
 </a>
-{#if figure.isMarked}
-	<a
-		style={`background-color: ${color};`}
-		class="card card-hover py-1 px-2 my-1 h-min flex justify-between relative hover:bg-surface-200-700-token"
-		href="/figures/{figure.id}"
-	>
-		<div class="flex">
-			<p class="sm:w-80 w-full sm:mr-0 mr-2 overflow-hidden sm:flex-shrink-0 break-all">
-				{figure.name}
-			</p>
-			{#if figure.expand.subSeriesId?.name}
-				<p class="text-surface-400 ml-4 mr-16 md:flex hidden overflow-y-hidden break-all">
-					{figure.expand.subSeriesId.name.substring(0, 30)}...
-				</p>
-			{/if}
-		</div>
-		<p class="w-max right-2 text-right flex-shrink-0">#{figure.mpgNr}</p>
-	</a>
-{:else}
-	<a
-		class="card card-hover py-1 px-2 my-1 h-min flex justify-between relative hover:bg-surface-200-700-token"
-		href="/figures/{figure.id}"
-	>
-		<div class="flex">
-			<p class="sm:w-80 w-full sm:mr-0 mr-2 overflow-hidden sm:flex-shrink-0 break-all">
-				{figure.name}
-			</p>
-			{#if figure.expand.subSeriesId?.name}
-				<p class="text-surface-400 ml-4 mr-16 md:flex hidden overflow-y-hidden break-all">
-					{figure.expand.subSeriesId.name.substring(0, 30)}...
-				</p>
-			{/if}
-		</div>
-		<p class="w-max right-2 text-right flex-shrink-0">#{figure.mpgNr}</p>
-	</a>
-{/if}
