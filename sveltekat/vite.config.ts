e0a3eb63d9911de86ead2a3323cc6b986f4a7de4
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2024-08-27 17:54:21 +0200

    Swaped encodings from legacy files to UTF-8

--- sveltekat/vite.config.ts
+++ sveltekat/vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import compression from 'compression';
 import { defineConfig } from 'vite';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
-	plugins: [sveltekit()] // , purgeCss()
+	plugins: [sveltekit()], // , purgeCss()
 });
