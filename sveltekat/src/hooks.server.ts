656788fdf5f0d230042671971a11f9cde28f5711
LiamKrenn <k.liam1224@gmail.com>
2024-08-29 03:16:30 +0200

    potential 500 error fix

--- sveltekat/src/hooks.server.ts
+++ sveltekat/src/hooks.server.ts
@@ -11,54 +11,59 @@ export const handle: Handle = sequence(
 		signInUrl: '/sign-in'
 	}),
 	async ({ event, resolve }) => {
-		if (event.locals.auth == undefined || event.locals.auth.userId == null) {
-			return await resolve(event);
-		}
-
-		const userId = event.locals.auth.userId;
-		let pb_user = null;
-		try {
-			pb_user = await pbdata.collection('user').getFirstListItem<{
-				id: string;
-				sub: string;
-				email: string;
-				stripe_id: string;
-				clerk_id: string;
-			}>('clerk_id="' + userId + '"', {});
-		} catch (e) {
-      
-			const clerk_user = await clerkClient.users.getUser(userId);
-			const email = clerk_user.emailAddresses[0].emailAddress;
-
-			try {
-				// user exists, but clerk_id is not set - payed on stripe before creating account
-				pb_user = await pbdata.collection('user').getFirstListItem<{
-					id: string;
-					sub: string;
-					email: string;
-					stripe_id: string;
-					clerk_id: string;
-				}>('email="' + email + '"', {});
-				pb_user = await pbdata.collection('user').update(pb_user.id, {
-					clerk_id: userId
-				});
-			} catch (e) {
-				// user does not exist
-				pb_user = await pbdata.collection('user').create<{
-					id: string;
-					sub: string;
-					email: string;
-					stripe_id: string;
-					clerk_id: string;
-				}>({
-					clerk_id: userId,
-					sub: null,
-					email: email
-				});
-			}
-		}
-
-		event.locals.pb_user = structuredClone(pb_user);
+    try {
+      if (event.locals.auth == undefined || event.locals.auth.userId == null) {
+        return await resolve(event);
+      }
+  
+      const userId = event.locals.auth.userId;
+      let pb_user = null;
+      try {
+        pb_user = await pbdata.collection('user').getFirstListItem<{
+          id: string;
+          sub: string;
+          email: string;
+          stripe_id: string;
+          clerk_id: string;
+        }>('clerk_id="' + userId + '"', {});
+      } catch (e) {
+        
+        const clerk_user = await clerkClient.users.getUser(userId);
+        const email = clerk_user.emailAddresses[0].emailAddress;
+  
+        try {
+          // user exists, but clerk_id is not set - payed on stripe before creating account
+          pb_user = await pbdata.collection('user').getFirstListItem<{
+            id: string;
+            sub: string;
+            email: string;
+            stripe_id: string;
+            clerk_id: string;
+          }>('email="' + email + '"', {});
+          pb_user = await pbdata.collection('user').update(pb_user.id, {
+            clerk_id: userId
+          });
+        } catch (e) {
+          // user does not exist
+          pb_user = await pbdata.collection('user').create<{
+            id: string;
+            sub: string;
+            email: string;
+            stripe_id: string;
+            clerk_id: string;
+          }>({
+            clerk_id: userId,
+            sub: null,
+            email: email
+          });
+        }
+      }
+  
+      event.locals.pb_user = structuredClone(pb_user);
+    } catch (e) {
+      event.locals.pb_user = structuredClone(null);
+    }
+		
 
 		return await resolve(event);
 	}
