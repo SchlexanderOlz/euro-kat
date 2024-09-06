97f48fdb6f46aaefaf5c6f535c4ea9f191167d93
LiamKrenn <k.liam1224@gmail.com>
2024-09-06 22:45:28 +0200

    test auto deployment

--- sveltekat/src/app.d.ts
+++ sveltekat/src/app.d.ts
@@ -3,26 +3,26 @@
 // and what to do when importing types
 declare namespace App {
 	interface Locals {
-    auth: {
-      userId: string;
-      claims: {
-        azp: string;
-        exp: number;
-        iat: number;
-        iss: string;
-        nbf: number;
-        sid: string;
-        sub: string;
-      }
-    },
-    pb_user: {
-      id: string;
-      sub: string;
-      email: string;
-      stripe_id: string;
-      clerk_id: string;
-    } | null
-  }
+		auth: {
+			userId: string;
+			claims: {
+				azp: string;
+				exp: number;
+				iat: number;
+				iss: string;
+				nbf: number;
+				sid: string;
+				sub: string;
+			};
+		};
+		pb_user: {
+			id: string;
+			sub: string;
+			email: string;
+			stripe_id: string;
+			clerk_id: string;
+		} | null;
+	}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
